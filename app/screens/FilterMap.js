45043dafa33bee0a2b007da539c34e6220d2f18e
Scott Wainstock <scott.wainstock@gmail.com>
2018-05-27 13:17:41 -0700

    filtermap and listlocation stubs and navigation

---
+++ app/screens/FilterMap.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class FilterMap extends Component {
+  render(){
+    return(<View><Text>FilterMap Screen</Text></View>);
+  }
+}
+
+export default FilterMap;
---
+++ app/screens/ListLocations.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class ListLocations extends Component {
+  render(){
+    return(<View><Text>ListLocations Screen</Text></View>);
+  }
+}
+
+export default ListLocations;
