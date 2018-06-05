ae84d87872e0c6c0749974885b649af25ca15f32
Scott Wainstock <scott.wainstock@gmail.com>
2018-06-04 20:54:03 -0700

    FilterMap header navigation

--- app/screens/FilterMap.js
+++ app/screens/FilterMap.js
@@ -1,9 +1,78 @@
+import "../config/globals.js"
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { HeaderBackButton } from 'react-navigation';
 
 class FilterMap extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={ isLoading: true }
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
+      title: 'Filter',
+      headerRight: <Button title="Save" onPress={() => params.saveFilters()} />
+    };
+  };
+
+  componentDidMount() {
+    this.props.navigation.setParams({ saveFilters: this._saveFilters });
+  }
+
+  _saveFilters = () => {
+    console.log('user updated')
+  }
+
   render(){
-    return(<View><Text>FilterMap Screen</Text></View>);
+    return(
+      <View style={{flex: 1}}>
+        <View>
+            <View>
+              <Text>Machine</Text>
+            </View>
+            <View>
+              <Text>Machine Dropdown</Text>
+            </View>
+            <View>
+              <Text>Machine Inputbox</Text>
+            </View>
+        </View>
+        <View>
+            <View>
+              <Text>Location Type</Text>
+            </View>
+            <View>
+              <Text>Location Type Dropdown</Text>
+            </View>
+            <View>
+              <Text>Location Type Inputbox</Text>
+            </View>
+        </View>
+        <View>
+            <View>
+              <Text># Of Machines</Text>
+            </View>
+            <View>
+              <Text># Of Machines Radio</Text>
+            </View>
+        </View>
+        <View>
+            <View>
+              <Text>Operator</Text>
+            </View>
+            <View>
+              <Text>Operator Dropdown</Text>
+            </View>
+            <View>
+              <Text>Operator Inputbox</Text>
+            </View>
+        </View>
+      </View>
+    );
   }
 }
 
