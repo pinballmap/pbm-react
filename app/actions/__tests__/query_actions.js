7cb773d6b88dc10def2fb9deaf1784a3af8f9ed5
Beth <beth.poore@gmail.com>
2018-07-28 10:25:33 -0700

    100% test coverage for redux actions

---
+++ app/actions/__tests__/query_actions.js
@@ -0,0 +1,24 @@
+import * as queryActions from '../query_actions'
+import * as types from '../types'
+
+describe('testing query actions', () => {
+    it('dispatched the expected action when the query is updated', () => {
+        const query = 'my new query'
+        const expectedAction = { 
+            type: types.UPDATE_QUERY,
+            payload: query,
+        }
+
+        expect(queryActions.updateQuery(query)).toEqual(expectedAction)
+    })
+
+    it('dispatched the expected action when the location id is updated', () => {
+        const id = 678
+        const expectedAction = { 
+            type: types.SET_LOCATION_ID,
+            payload: id
+        }
+
+        expect(queryActions.setLocationId(id)).toEqual(expectedAction)
+    })
+})
