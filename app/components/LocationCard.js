826a0387642dfe2a3a8ae0928c6e90eba0e18453
ryantg <gratzer@posteo.org>
2018-08-08 08:45:21 -0700

    line breaks on machine list

--- app/components/LocationCard.js
+++ app/components/LocationCard.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Card } from 'react-native-elements'
 import { Ionicons } from '@expo/vector-icons';
 
 class LocationCard extends Component {
   render(){
-    const machineList = this.props.machines.join(' , ')
+    const machineList = this.props.machines.join('\n')
     machineList.slice(0, -2)
 
     return(
         <Card style={{flex: 1}}>
           <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'space-around'}}>
-            <View >
-              <Text>{this.props.name}</Text>
+            <View>
+              <Text style={s.locationName}>{this.props.name}</Text>
               <Text numberOfLines={1} ellipsizeMode={'tail'}>{`${this.props.street}, ${this.props.state} ${this.props.zip}`}</Text>
-              <Text>{this.props.type} {this.props.distance.toFixed(2)} mi</Text>
+              <Text>{this.props.type}</Text> 
+              <Text>{this.props.distance.toFixed(2)} mi</Text>
               <Text>{machineList}</Text>
             </View>
             <Ionicons name="ios-arrow-dropright" size={24} onPress={() => this.props.navigation.navigate('LocationDetails', {id: this.props.id, type: this.props.type, context: this.props.context})}/>
@@ -23,5 +24,11 @@ class LocationCard extends Component {
     );
   }
 }
+
+const s = StyleSheet.create({
+  locationName: {
+    fontWeight: 'bold'
+  }
+});
 
 export default LocationCard;
