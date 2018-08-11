0e3485e52328066e7f3f621afc4658218c8625b2
ryantg <gratzer@posteo.org>
2018-08-11 09:32:01 -0700

    accidentally removed something

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -18,6 +18,8 @@ class Login extends Component {
 
   }
 
+  static navigationOptions = { header: null };
+  
   submit = () => {
     this.setState({
       errors: false,
@@ -107,7 +109,7 @@ class Login extends Component {
               >{"I forgot my password"}
             </Text>
             <Text
-              //onPress={() => this.props.navigation.navigate('Confirm')}
+              //onPress={() => this.props.navigation.navigate('Confirm  ')}
               style={s.textLink}
               >{"Resend my confirmation email"}
             </Text>
