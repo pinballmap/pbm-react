c9435b013d57a27b12bf48965abe2c3a51842907
ryantg <gratzer@posteo.org>
2018-08-31 20:47:58 -0700

    updated bottom nav screen titles

--- app/screens/Saved.js
+++ app/screens/Saved.js
@@ -14,7 +14,7 @@ class RecentMachines extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
       headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
-      title: 'LocationList',
+      title: 'Saved',
     };
   };
 
