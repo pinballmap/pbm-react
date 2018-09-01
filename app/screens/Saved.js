ada85428959f3fdebfa8398dab7dc3e864c0f08b
Beth <beth.poore@gmail.com>
2018-08-31 18:50:19 -0700

    Have tab navigation items back action = Map view

--- app/screens/Saved.js
+++ app/screens/Saved.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Text, View } from 'react-native';
+import { HeaderBackButton } from 'react-navigation';
 
 class RecentMachines extends Component {
   constructor(props) {
@@ -9,6 +10,14 @@ class RecentMachines extends Component {
       machines: this.props.machines.machines
     }
   }
+  
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
+      title: 'LocationList',
+    };
+  };
+
 // I just copied the recentmachines screen code as a placeholder. 
 // But this screen should be like LocationList, but only showing SAVED locations.
   render(){
