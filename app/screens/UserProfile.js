dc567cdff5b0754c2614d6a4571acd81dcb1e622
Beth <beth.poore@gmail.com>
2018-08-31 18:50:19 -0700

    Have tab navigation items back action = Map view

--- app/screens/UserProfile.js
+++ app/screens/UserProfile.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { HeaderBackButton } from 'react-navigation';
 
 class UserProfile extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
+      title: 'LocationList',
+    };
+  };
+  
   render(){
     return(<View><Text>UserProfile Screen</Text></View>);
   }
