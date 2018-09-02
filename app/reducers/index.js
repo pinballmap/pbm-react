449bdb7399c211c6dba0f8c4a998dcab6530705d
Beth <beth.poore@gmail.com>
2018-09-02 16:41:41 -0700

    Fetch operators to application status. Display on location details screen.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,10 +3,12 @@ import query from './queries'
 import locations from './locations'
 import machines from './machines'
 import user from './user'
+import operators from './operators'
 
 export default combineReducers({
     query,
     locations,
     machines,
     user,
+    operators,
 })
