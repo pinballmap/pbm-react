d62579fd1b4abc25d417908c916a336da7b797f4
ryantg <gratzer@posteo.org>
2018-09-02 09:24:43 -0700

    locationcard: location name width; and touchableopacity so whole card is clickable

--- app/components/LocationCard.js
+++ app/components/LocationCard.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-elements'
 import { Ionicons } from '@expo/vector-icons';
 
 class LocationCard extends Component {
   render(){
     const numMachines = this.props.machines.length
-    const machineList = numMachines <= 5 ? this.props.machines.join('\n') : `${this.props.machines.slice(0, 5).join('\n')}. \nPlus ${numMachines - 5} more!`
+    const machineList = numMachines <= 5 ? this.props.machines.join('\n') : `${this.props.machines.slice(0, 5).join('\n')} \nPlus ${numMachines - 5} more!`
 
     return(
         <Card style={{flex: 1}}>
-          <View style={s.flexi}>
-            <View style={{width: '95%'}}>
-              <View style={s.locationNameContainer}>
-                <Text style={s.locationName}>{this.props.name}</Text>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('LocationDetails', {id: this.props.id, locationName: this.props.name})}>
+            <View style={s.flexi}>
+              <View style={{width: '95%'}}>
+                <View style={s.locationNameContainer}>
+                  <Text style={s.locationName}>{this.props.name}</Text>
+                </View>
+                <Text numberOfLines={1} ellipsizeMode={'tail'}>{`${this.props.street}, ${this.props.state} ${this.props.zip}`}</Text>
+                {this.props.type && <Text>Location Type: {this.props.type}</Text>}
+                <Text style={s.margin}>Distance: {this.props.distance.toFixed(2)} mi</Text>
+                <Text style={[s.margin, s.bold]}>{machineList}</Text>
               </View>
-              <Text numberOfLines={1} ellipsizeMode={'tail'}>{`${this.props.street}, ${this.props.state} ${this.props.zip}`}</Text>
-              {this.props.type && <Text>Location Type: {this.props.type}</Text>}
-              <Text style={s.margin}>Distance: {this.props.distance.toFixed(2)} mi</Text>
-              <Text style={[s.margin, s.bold]}>{machineList}</Text>
+              <Ionicons style={s.iconStyle} name="ios-arrow-dropright"/>
             </View>
-            <Ionicons style={s.iconStyle} name="ios-arrow-dropright" onPress={() => this.props.navigation.navigate('LocationDetails', {id: this.props.id, locationName: this.props.name})}/>
-          </View>
+          </TouchableOpacity>
         </Card>
     );
   }
@@ -35,7 +37,7 @@ const s = StyleSheet.create({
     alignContent: 'space-around',
   },
   locationNameContainer: {
-    width: '100%',
+    width: '105%',
     backgroundColor: "#D3ECFF",
     marginBottom: 10,
     padding: 5,
