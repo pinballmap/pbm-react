d60715691ac4a665ecef5efee6f6d49d2a294b79
Beth Poore <bpoore@discogsinc.com>
2018-10-07 15:22:29 -0700

    Log out button on User Profile screen.

--- app/screens/UserProfile.js
+++ app/screens/UserProfile.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AsyncStorage, Modal, Text,  View } from 'react-native';
 import { HeaderBackButton } from 'react-navigation';
+import { Button } from 'react-native-elements';
 
 class UserProfile extends Component {
+  state = {
+    modalVisible: false,
+  }
+
   static navigationOptions = ({ navigation }) => {
     return {
       headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
       title: 'Profile',
     };
   };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible})
+  }
   
   render(){
-    return(<View><Text>UserProfile Screen</Text></View>);
+    return(
+      <View>
+        <Text>UserProfile Screen</Text>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+        >
+          <View style={{marginTop: 100}}>
+            <View>
+
+              <Button
+                title={"Logout?"}
+                onPress={() => {
+                  AsyncStorage.removeItem('auth')
+                  this.setModalVisible(!this.state.modalVisible)
+                }} 
+              />
+              <Button
+                title={"Cancel"}
+                onPress={() => this.setModalVisible(!this.state.modalVisible)}
+              />
+            </View>
+          </View>
+        </Modal>
+
+        <Button
+          title={"Logout"} 
+          onPress={() => this.setModalVisible(true)}
+        />
+      </View>
+    );
   }
 }
 
