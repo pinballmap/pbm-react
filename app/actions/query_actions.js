6dc84d3060f09dbfe4e3b9511fbc11e4c81375a1
Beth Poore <bpoore@discogsinc.com>
2018-10-07 16:28:22 -0700

    Add functionality to include distance in LocationDetails. Start addressing found bug with distance calcs for LocationList

--- app/actions/query_actions.js
+++ app/actions/query_actions.js
@@ -9,7 +9,7 @@ import {
 
 export const updateQuery = payload => ({ type: UPDATE_QUERY, payload })
 export const setLocationId = (id, name) => ({ type: SET_LOCATION_ID, id, name })
-export const updateCurrCoordindates = (lat, lon, latDelta, lonDelta) =>  ({ type: UPDATE_COORDINATES, lat, lon, latDelta, lonDelta }) 
+export const updateCurrCoordindates = (lat, lon, latDelta = 0.1, lonDelta = 0.1) =>  ({ type: UPDATE_COORDINATES, lat, lon, latDelta, lonDelta }) 
 export const setSelectedMachine = machineId => ({ type: SET_SELECTED_MACHINE, machineId })
 export const setSelectedLocationType = locationType => ({ type: SET_SELECTED_LOCATION_TYPE, locationType })
 export const setSelectedNumMachines = num => ({ type: SET_SELECTED_NUM_MACHINES, num })
---
+++ app/utils/utilityFunctions.js
@@ -0,0 +1,22 @@
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in miles)
+export function getDistance(lat1, lon1, lat2, lon2) 
+{
+    var R = 3956; // miles
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
+
+    return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
