0fa1ce381c9a58be2627b14de4278c4d66c25e97
ryantg <gratzer@posteo.org>
2018-10-11 17:26:45 -0700

    added placeholder About, FAQ, and Podcast screens

--- app/screens/About.js
+++ app/screens/About.js
@@ -3,24 +3,22 @@ import { connect } from 'react-redux';
 import { Text, View } from 'react-native';
 import { HeaderBackButton } from 'react-navigation';
 
-class SavedLocations extends Component {
+class Podcast extends Component {
   
   static navigationOptions = ({ navigation }) => {
     return {
       headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
-      title: 'Saved',
+      title: 'Podcast',
     };
   };
 
-// I just copied the recentmachines screen code as a placeholder. 
-// But this screen should be like LocationList, but only showing SAVED locations.
   render(){
     return(
       <View>
-        <Text>Saved</Text>
+        <Text>placeholder</Text>
       </View>);
   }
 }
 
 const mapStateToProps = ({ machines }) => ({ machines })
-export default connect(mapStateToProps)(SavedLocations);
+export default connect(mapStateToProps)(Podcast);
