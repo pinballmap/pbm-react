bf1ccc10173c998b4bb0252fd91e484ea55b45dd
ryantg <gratzer@posteo.org>
2018-10-12 08:43:15 -0700

    resolved missing proptypes variable error

--- app/screens/FilterMap.js
+++ app/screens/FilterMap.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Button, Picker, Text, View } from 'react-native'
 import { ButtonGroup } from 'react-native-elements'
