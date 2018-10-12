9079ffe28fde8309e57b89d86a9e7010a145a196
ryantg <gratzer@posteo.org>
2018-10-12 15:09:23 -0700

    changed color of back nav button

--- app/screens/FAQ.js
+++ app/screens/FAQ.js
@@ -7,7 +7,7 @@ class FAQ extends Component {
   
   static navigationOptions = ({ navigation }) => {
       return {
-          headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
+          headerLeft: <HeaderBackButton tintColor="#888888" onPress={() => navigation.goBack(null)} title="Map" />,
           title: 'FAQ',
       }
   };
--- app/screens/Saved.js
+++ app/screens/Saved.js
@@ -8,7 +8,7 @@ class SavedLocations extends Component {
   
   static navigationOptions = ({ navigation }) => {
       return {
-          headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
+          headerLeft: <HeaderBackButton tintColor="#888888" onPress={() => navigation.goBack(null)} title="Map" />,
           title: 'Saved',
       }
   };
