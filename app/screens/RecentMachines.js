1290bd8a5376edb229b6fb06f834c49cf379fe64
Beth Poore <bpoore@discogsinc.com>
2018-10-11 20:27:47 -0700

    Update linting rules and script to run linter

--- app/screens/RecentMachines.js
+++ app/screens/RecentMachines.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Text, View } from 'react-native'
 
 class RecentMachines extends Component {
-  constructor(props) {
-    super(props) 
-    this.state = {
-      machines: this.props.machines.machines
+    constructor(props) {
+        super(props) 
+        this.state = {
+            machines: this.props.machines.machines
+        }
     }
-  }
 
-  render(){
-    return(
-      <View>
-        <Text>MACHINES</Text>
-        {this.state.machines.map(machine => <Text key={machine.id}>{machine.name}</Text>)}
-      </View>);
-  }
+    render(){
+        return(
+            <View>
+                <Text>MACHINES</Text>
+                {this.state.machines.map(machine => <Text key={machine.id}>{machine.name}</Text>)}
+            </View>)
+    }
 }
 
 const mapStateToProps = ({ machines }) => ({ machines })
-export default connect(mapStateToProps)(RecentMachines);
+export default connect(mapStateToProps)(RecentMachines)
