d15555b143cc6ca9b64f0dc94120c7c52c770efe
Beth Poore <bpoore@discogsinc.com>
2018-10-13 15:06:01 -0700

    Drawer menu first pass

--- app/screens/About.js
+++ app/screens/About.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Text, View } from 'react-native'
-import { HeaderBackButton } from 'react-navigation'
+import PropTypes from 'prop-types'
+import { Button, Text, View } from 'react-native'
 
-class Podcast extends Component {
+class About extends Component {
   
-  static navigationOptions = ({ navigation }) => {
-      return {
-          headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} title="Map" />,
-          title: 'Podcast',
-      }
-  };
-
+  static navigationOptions = { drawerLabel: 'About' }
+     
   render(){
       return(
-          <View>
-              <Text>placeholder</Text>
+          <View style={{marginTop: 300, flex: 1}}>
+              <Button
+                  onPress={ () => this.props.navigation.navigate('Map') }
+                  style={{width:30, paddingTop: 15}}
+                  title="Back to Map"
+              />
+              <Text>About</Text>
           </View>)
   }
 }
 
-const mapStateToProps = ({ machines }) => ({ machines })
-export default connect(mapStateToProps)(Podcast)
+About.propTypes = {
+    navigation: PropTypes.object,
+}
+
+export default About
--- app/screens/Donate.js
+++ app/screens/Donate.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Button, Text, View } from 'react-native'
 
-class FAQ extends Component {
+class Donate extends Component {
   
-  static navigationOptions = { drawerLabel: 'FAQ' }
+  static navigationOptions = { drawerLabel: 'Donate' }
      
   render(){
       return(
@@ -14,13 +14,13 @@ class FAQ extends Component {
                   style={{width:30, paddingTop: 15}}
                   title="Back to Map"
               />
-              <Text>FAQ</Text>
+              <Text>Donate</Text>
           </View>)
   }
 }
 
-FAQ.propTypes = {
+Donate.propTypes = {
     navigation: PropTypes.object,
 }
 
-export default FAQ
+export default Donate
