7c219f46d733d545012aec6b927fedbb8c03c97d
Beth Poore <bpoore@discogsinc.com>
2018-10-16 17:05:06 -0700

    File formatting

--- app/screens/Podcast.js
+++ app/screens/Podcast.js
@@ -14,64 +14,64 @@ class Podcast extends Component {
   }
 
   render(){
-    return(
-      <ScrollView style={{flex:1}}>
-        <View style={s.container}>
-          <View style={[s.logoWrapper,s.child]}>
-            <Image source={require('../assets/images/mappin-logo-600.png')} style={s.logo}/>
-          </View>
-          <View style={s.child}>
-            <Text style={s.text}>In Summer 2018 we started a podcast, <Text style={s.bold}>Mappin’ Around w/ Scott and Ryan</Text>! We talk about site news, site tech, stats, and we interview operators and friends. We release a new episode once a month. Check it out!</Text>
-            <Text style={s.textLink}
-              onPress={() => Linking.openURL('http://pod.pinballmap.com')}
-            >pod.pinballmap.com</Text>
-          </View>  
-        </View>
-      </ScrollView>
-    )
+      return(
+          <ScrollView style={{flex:1}}>
+              <View style={s.container}>
+                  <View style={[s.logoWrapper,s.child]}>
+                      <Image source={require('../assets/images/mappin-logo-600.png')} style={s.logo}/>
+                  </View>
+                  <View style={s.child}>
+                      <Text style={s.text}>In Summer 2018 we started a podcast, <Text style={s.bold}>Mappin’ Around w/ Scott and Ryan</Text>! We talk about site news, site tech, stats, and we interview operators and friends. We release a new episode once a month. Check it out!</Text>
+                      <Text style={s.textLink}
+                          onPress={() => Linking.openURL('http://pod.pinballmap.com')}
+                      >pod.pinballmap.com</Text>
+                  </View>  
+              </View>
+          </ScrollView>
+      )
   }
 }
 
 const s = StyleSheet.create({
-  container: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    flex: 1,
-  },
-  logoWrapper: {
-    padding: 5,
-    flex: 2
-  },
-  logo: {
-    width: 300,
-    height: 300,
-    justifyContent: 'center',    
-  },
-  child: {
-    margin: "auto",
-    padding: 10,
-  },
-  text: {
-    fontSize: 16
-  },
-  bold: {
-    fontWeight: 'bold',
-  },
-  textLink: {
-    textDecorationLine: 'underline',
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 5,
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: "#D3ECFF",
-    fontWeight: 'bold'
-  }
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        flex: 1,
+    },
+    logoWrapper: {
+        padding: 5,
+        flex: 2
+    },
+    logo: {
+        width: 300,
+        height: 300,
+        justifyContent: 'center',    
+    },
+    child: {
+        margin: "auto",
+        padding: 10,
+    },
+    text: {
+        fontSize: 16
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+    textLink: {
+        textDecorationLine: 'underline',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 5,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: "#D3ECFF",
+        fontWeight: 'bold'
+    }
 })
 
 Podcast.propTypes = {
-  navigation: PropTypes.object,
+    navigation: PropTypes.object,
 }
 
 export default Podcast
