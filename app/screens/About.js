1e586e7e6590e1bdf1a7aef769376c92d55e946d
Beth Poore <bpoore@discogsinc.com>
2018-10-16 17:05:06 -0700

    File formatting

--- app/screens/About.js
+++ app/screens/About.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Button, Text, View, StyleSheet, ScrollView, Image, Linking } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, Image, Linking } from 'react-native'
 import { HeaderBackButton } from 'react-navigation'
 import { getData } from '../config/request'
 
 class About extends Component {
-    constructor(props){
-        super(props)
-
-        this.state ={ 
-            num_locations: 0, 
-            num_lmxes: 0, 
-            apiError: '',
-        }
+    state ={ 
+        num_locations: 0, 
+        num_lmxes: 0, 
+        apiError: '',
     }
 
     static navigationOptions = ({ navigation }) => {
@@ -45,12 +41,12 @@ class About extends Component {
             <ScrollView style={{flex:1}}>
                 <View style={s.container}>
                     <View style={[s.logoWrapper,s.child]}>
-                         <Image source={require('../assets/images/pinballmapcom_nocom.png')} resizeMode="contain" style={s.logo}/>
+                        <Image source={require('../assets/images/pinballmapcom_nocom.png')} resizeMode="contain" style={s.logo}/>
                     </View>
                     <View style={s.child}>
                         <Text style={s.text}>This map lists all the public pinball machines in North America and beyond. It is currently listing {this.state.num_locations} locations and {this.state.num_lmxes} machines. The data is kept up to date by YOU! You can update it using this app or the website: <Text style={s.textLink} onPress={() => Linking.openURL('https://pinballmap.com')}>pinballmap.com</Text>.</Text>
                         <Text style={s.text}><Text onPress={ () => this.props.navigation.navigate('Contact') } style={s.textLink}>{"Contact Us"}</Text>. <Text onPress={ () => this.props.navigation.navigate('Blog') } style={s.textLink}>{"Read the blog"}</Text>, or <Text onPress={ () => this.props.navigation.navigate('Blog') } style={s.textLink}>{"read the FAQ"}</Text>.</Text>
-                        <Text style={s.text}>Listen to our podcast, <Text style={s.textLink} onPress={() => Linking.openURL('http://pod.pinballmap.com')}>Mappin' Around with Scott & Ryan!</Text></Text>
+                        <Text style={s.text}>Listen to our podcast, <Text style={s.textLink} onPress={() => Linking.openURL('http://pod.pinballmap.com')}>{`Mappin' Around with Scott & Ryan!`}</Text></Text>
                         <Text style={s.text}>Follow <Text style={s.textLink} onPress={() => Linking.openURL('https://twitter.com/pinballmapcom')}>@pinballmapcom</Text> on Twitter for updates and news!</Text>
                         <Text style={s.text}>Support us via the <Text style={s.textLink} onPress={() => Linking.openURL('https://patreon.com/pinballmap')}>Pinball Map Patreon</Text>.</Text>
                         <Text style={s.text}>We have a couple of <Text style={s.textLink} onPress={() => Linking.openURL('https://pinballmap.com/store')}>shirts for sale</Text>.</Text>
@@ -68,10 +64,10 @@ class About extends Component {
 
 const s = StyleSheet.create({
     container: {
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        flex: 1,
     },
     logoWrapper: {
         padding: 5,
@@ -81,24 +77,24 @@ const s = StyleSheet.create({
         flex:1, height: 60, width: 300    
     },
     child: {
-      margin: "auto",
-      padding: 10,
+        margin: "auto",
+        padding: 10,
     },
     text: {
-      fontSize: 16,
-      marginBottom: 10
+        fontSize: 16,
+        marginBottom: 10
     },
     bold: {
-      fontWeight: 'bold',
-      fontSize: 18,
-      marginBottom: 10
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 10
     },
     textLink: {
         textDecorationLine: 'underline',
         color: "#F53240",
         fontSize: 16,
     },
-  })
+})
 
 About.propTypes = {
     navigation: PropTypes.object,
--- app/screens/FAQ.js
+++ app/screens/FAQ.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Button, Text, View, StyleSheet, ScrollView } from 'react-native'
+import { Text, View, StyleSheet, ScrollView } from 'react-native'
 import { HeaderBackButton } from 'react-navigation'
 
 class FAQ extends Component {
@@ -14,48 +14,48 @@ class FAQ extends Component {
     }
      
     render(){
-      return(
-        <ScrollView style={{flex:1}}>
-          <View style={s.container}>
-            <View style={s.child}>
-              <Text style={s.bold}>How do I add a new location?</Text>
-              <Text style={s.text}>Click the menu icon in the lower left, and choose "Add Location". Then fill out the form! We moderate the submissions, so it will a few days for the location you submitted to be added to the map. The more accurate and thorough your submission, the quicker it will get added! Make sure to include at least one machine with your submission.</Text>
-              <Text style={s.bold}>This machine is no longer at this location.</Text>
-              <Text style={s.text}>This is a user-powered map, and YOU can remove the machine from the location. Click on the machine name, and then look for a "remove" button.</Text>
-              <Text style={s.bold}>This location closed/no longer has machines.</Text>
-              <Text style={s.text}>Simply remove all the machines from it. Empty locations are periodically removed.</Text>
-              <Text style={s.bold}>Have a question that we didn't cover here? <Text onPress={ () => this.props.navigation.navigate('Contact') } style={s.textLink}>{"Ask us!"}</Text></Text>
-            </View>  
-          </View>
-        </ScrollView>
-      )
+        return(
+            <ScrollView style={{flex:1}}>
+                <View style={s.container}>
+                    <View style={s.child}>
+                        <Text style={s.bold}>{`How do I add a new location?`}</Text>
+                        <Text style={s.text}>{`Click the menu icon in the lower left, and choose "Add Location". Then fill out the form! We moderate the submissions, so it will a few days for the location you submitted to be added to the map. The more accurate and thorough your submission, the quicker it will get added! Make sure to include at least one machine with your submission.`}</Text>
+                        <Text style={s.bold}>{`This machine is no longer at this location.`}</Text>
+                        <Text style={s.text}>{`This is a user-powered map, and YOU can remove the machine from the location. Click on the machine name, and then look for a "remove" button.`}</Text>
+                        <Text style={s.bold}>{`This location closed/no longer has machines.`}</Text>
+                        <Text style={s.text}>{`Simply remove all the machines from it. Empty locations are periodically removed.`}</Text>
+                        <Text style={s.bold}>{`Have a question that we didn't cover here?`} <Text onPress={ () => this.props.navigation.navigate('Contact') } style={s.textLink}>{"Ask us!"}</Text></Text>
+                    </View>  
+                </View>
+            </ScrollView>
+        )
     }
 }
 
 const s = StyleSheet.create({
-  container: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    flex: 1,
-  },
-  child: {
-    margin: "auto",
-    padding: 10,
-  },
-  text: {
-    fontSize: 16,
-    marginBottom: 10
-  },
-  bold: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginBottom: 10
-  },
-  textLink: {
-    textDecorationLine: 'underline',
-    color: "#F53240",
-  },
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        flex: 1,
+    },
+    child: {
+        margin: "auto",
+        padding: 10,
+    },
+    text: {
+        fontSize: 16,
+        marginBottom: 10
+    },
+    bold: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 10
+    },
+    textLink: {
+        textDecorationLine: 'underline',
+        color: "#F53240",
+    },
 })
 
 FAQ.propTypes = {
