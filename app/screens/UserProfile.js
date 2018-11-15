b38608284f4cc917e3f09d32f0ff54d71ac4890f
Beth Poore <bpoore@discogsinc.com>
2018-11-14 16:50:08 -0800

    User Profile skeleton

--- app/screens/UserProfile.js
+++ app/screens/UserProfile.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Modal, Text, View, StyleSheet } from 'react-native'
+import { ActivityIndicator, Modal, Text, View, StyleSheet, ScrollView } from 'react-native'
 import { HeaderBackButton } from 'react-navigation'
-import { Button } from 'react-native-elements'
+import { Button, ListItem } from 'react-native-elements'
+import { getData } from '../config/request'
 import { logout } from '../actions/user_actions'
+
+const moment = require('moment')
 
 class UserProfile extends Component {
   state = {
       modalVisible: false,
+      fetchingUserInfo: this.props.user.loggedIn ? true : false,
+      profile_info: {},
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -21,9 +26,28 @@ class UserProfile extends Component {
   setModalVisible(visible) {
       this.setState({modalVisible: visible})
   }
-  
+
+  componentDidMount() {
+      const id = this.props.user.id
+      if (id) {
+          getData(`/users/${id}/profile_info.json`)
+              .then(data => {
+                  this.setState({
+                      fetchingUserInfo: false,
+                      profile_info: data.profile_info,
+                  })
+              })
+      }
+  }
+
   render(){
+      if (this.state.fetchingUserInfo)
+          return <ActivityIndicator />
+
       const { user } = this.props
+      const profileInfo = this.state.profile_info
+      console.log(this.state)
+
       return(
           <View>
               <Text style={s.pageTitle}>User Profile</Text>
@@ -70,9 +94,38 @@ class UserProfile extends Component {
               </Modal>
 
               {user.loggedIn ?
-                  <View>
+                  <ScrollView>
                       <Text>{user.username}</Text>
-                      {/* Need to make API call for rest of the data to /api/v1/users/{user.id}/profile_info.json */}
+                      <Text>{`Member since: ${moment(profileInfo.created_at).format('MMM-DD-YYYY')}`}</Text>
+                      <Text>{`Machines Added: ${profileInfo.num_machines_added}`}</Text>
+                      <Text>{`Machines Removed: ${profileInfo.num_machines_removed}`}</Text>
+                      <Text>{`Machines Comments: ${profileInfo.num_lmx_comments_left}`}</Text>
+                      <Text>{`Locations submitted: ${profileInfo.num_locations_suggested}`}</Text>
+                      <Text>{`Locations edited: ${profileInfo.num_locations_edited}`}</Text>
+                      <Button 
+                          title={'Saved Locations'}
+                          onPress={() => this.props.navigation.navigate('Saved')}
+                      />
+                      <Text>Locations Edited:</Text>
+                      <View style={{backgroundColor:'#ffffff',paddingTop:5,paddingBottom:5}}>
+                          {profileInfo.profile_list_of_edited_locations.slice(0, 50).map(location => {
+                              return <ListItem
+                                  key={location[0]}
+                                  titleStyle={{marginLeft:15,marginRight:15}}
+                                  title={location[1]}
+                              /> 
+                          })}
+                      </View>
+                      <Text>High Scores:</Text>
+                      <View style={{backgroundColor:'#ffffff',paddingTop:5,paddingBottom:5}}>
+                          {profileInfo.profile_list_of_high_scores.map((score, idx) => {
+                              return <ListItem
+                                  key={`${score[0]}-${score[1]}-${score[2]}-${score[3]}-${idx}`}
+                                  titleStyle={{marginLeft:15,marginRight:15}}
+                                  title={`${score[2]} on ${score[1]} at ${score[0]} on ${score[3]}`}
+                              /> 
+                          })}
+                      </View>
                       <Button
                           title={"Logout"} 
                           onPress={() => this.setModalVisible(true)}
@@ -86,7 +139,7 @@ class UserProfile extends Component {
                           style={{borderRadius: 50}}
                           containerStyle={{borderRadius:50,marginLeft:15,marginRight:15}}
                       /> 
-                  </View> :
+                  </ScrollView> :
                   <View>
                       <Text style={s.hiya}>{`Hi, you're not logged in, so you don't have a profile!`}</Text>
                       <Button
