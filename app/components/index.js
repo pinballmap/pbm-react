e8191e6eecdbc9c3d5e8858466b12dc4c00c011f
Beth Poore <bpoore@discogsinc.com>
2019-02-08 09:21:35 -0800

    Remove unused component

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,6 @@
 import LocationCard from './LocationCard'
 import RemoveMachine from './RemoveMachine'
 import RemoveMachineModal from './RemoveMachineModal'
-import SaveAddMachine from './SaveAddMachine'
 import SearchBar from './SearchBar'
 import NotLoggedIn from './NotLoggedIn'
 import ConfirmationModal from './ConfirmationModal'
@@ -10,7 +9,6 @@ export {
     LocationCard,
     RemoveMachine,
     RemoveMachineModal,
-    SaveAddMachine,
     SearchBar,
     NotLoggedIn,
     ConfirmationModal,
