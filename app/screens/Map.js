b35f70f88c4a4fd1e1f9e4caa0373eb0dedcf1cc
ryantg <gratzer@posteo.org>
2019-02-20 22:19:52 -0800

    move get location button from searchbar to map

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Font } from 'expo'
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
+import { Button, Icon } from 'react-native-elements'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import { SearchBar } from '../components'
 import { 
@@ -18,6 +19,7 @@ import {
 } from '../actions'
 import { retrieveItem } from '../config/utils'
 import { Ionicons } from '@expo/vector-icons'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 class Map extends Component {
     constructor(props){
@@ -39,10 +41,10 @@ class Map extends Component {
         }
     }
 
-  static navigationOptions = ({ navigation }) => {  
-      return {
-          drawerLabel: 'Map',
-          headerLeft:
+    static navigationOptions = ({ navigation }) => {  
+        return {
+            drawerLabel: 'Map',
+            headerLeft:
         <Button
             onPress={ () => navigation.navigate('LocationList') }
             containerStyle={{width:50}}
@@ -51,10 +53,10 @@ class Map extends Component {
             titleStyle={s.titleStyle}
             clear={true}
         />,
-          headerTitle:
+            headerTitle:
         <SearchBar
         />,
-          headerRight:
+            headerRight:
         <Button
             onPress={ () => navigation.navigate('FilterMap')}
             containerStyle={{width:60}}
@@ -63,176 +65,189 @@ class Map extends Component {
             titleStyle={s.titleStyle}
             clear={true}
         />
-      }
-  };
+        }
+    };
 
-  reloadMap() { 
-      const { machineId, locationType, numMachines, selectedOperator } = this.props.query
-      const machineQueryString = machineId ? `by_machine_id=${machineId};` : ''
-      const locationTypeQueryString = locationType ? `by_type_id=${locationType};` : ''
-      const numMachinesQueryString = numMachines ? `by_at_least_n_machines_type=${numMachines};` : ''
-      const byOperator = selectedOperator ? `by_operator_id=${selectedOperator};` : ''
-      this.props.getLocations(`/locations/closest_by_lat_lon.json?lat=${this.state.region.latitude};lon=${this.state.region.longitude};${machineQueryString}${locationTypeQueryString}${numMachinesQueryString}${byOperator}max_distance=5;send_all_within_distance=1`, true)  
-  }
+    reloadMap() { 
+        const { machineId, locationType, numMachines, selectedOperator } = this.props.query
+        const machineQueryString = machineId ? `by_machine_id=${machineId};` : ''
+        const locationTypeQueryString = locationType ? `by_type_id=${locationType};` : ''
+        const numMachinesQueryString = numMachines ? `by_at_least_n_machines_type=${numMachines};` : ''
+        const byOperator = selectedOperator ? `by_operator_id=${selectedOperator};` : ''
+        this.props.getLocations(`/locations/closest_by_lat_lon.json?lat=${this.state.region.latitude};lon=${this.state.region.longitude};${machineQueryString}${locationTypeQueryString}${numMachinesQueryString}${byOperator}max_distance=5;send_all_within_distance=1`, true)  
+    }
 
-  onRegionChange = (region) => {
-      //Only reload map if the location hasn't moved in 0.5sec
-      const compareRegion = (region) => {
-          if (region === this.prevRegion) {
-              this.setState({ 
-                  region, 
-                  locations: [], 
-              })
-              this.props.updateCoordinates(region.latitude, region.longitude, region.latitudeDelta, region.longitudeDelta)
-              this.reloadMap()
-          }
-      }
-  
-      setTimeout(compareRegion, 500, region)
-      this.prevRegion = region
-  }
+    onRegionChange = (region) => {
+        //Only reload map if the location hasn't moved in 0.5sec
+        const compareRegion = (region) => {
+            if (region === this.prevRegion) {
+                this.setState({ 
+                    region, 
+                    locations: [], 
+                })
+                this.props.updateCoordinates(region.latitude, region.longitude, region.latitudeDelta, region.longitudeDelta)
+                this.reloadMap()
+            }
+        }
 
-  componentDidUpdate(){
-      if (this.mapRef) {
-          setTimeout(() => this.mapRef.fitToElements(true), 1000)  
-      }
-  }
+        setTimeout(compareRegion, 500, region)
+        this.prevRegion = region
+    }
 
-  componentDidMount(){
-      retrieveItem('auth').then((auth) => {
-          if (!auth && !this.props.user.loginLater) {
-              this.props.navigation.navigate('SignupLogin')
-          }
-          else if (auth) {
-              this.setState({ authCheck: true })
-              this.props.login(auth)
-          } 
-          else {
-              this.setState({ authCheck: true })
-          }
-      }).catch((error) => console.log('Promise is rejected with error: ' + error)) 
+    componentDidUpdate(){
+        if (this.mapRef) {
+            setTimeout(() => this.mapRef.fitToElements(true), 1000)  
+        }
+    }
 
-      this.props.getCurrentLocation()
-      this.props.getLocationTypes('/location_types.json')
-      this.props.getMachines('/machines.json')
-      this.props.getOperators('/operators.json')
-  }
+    componentDidMount(){
+    Font.loadAsync({'FontAwesome': require('@expo/vector-icons/fonts/FontAwesome.ttf')})
+        retrieveItem('auth').then((auth) => {
+            if (!auth && !this.props.user.loginLater) {
+                this.props.navigation.navigate('SignupLogin')
+            }
+            else if (auth) {
+                this.setState({ authCheck: true })
+                this.props.login(auth)
+            } 
+            else {
+                this.setState({ authCheck: true })
+            }
+        }).catch((error) => console.log('Promise is rejected with error: ' + error)) 
 
-  UNSAFE_componentWillReceiveProps(props) {
-      const { machineId, locationType, numMachines, selectedOperator, curLat, curLon, latDelta, lonDelta, locationId, locationName } = props.query
-      if (!this.props.user.lat && props.user.lat) {
-          this.setState({
-              region: {
-                  ...this.state.region,
-                  latitude: props.user.lat, 
-                  longitude: props.user.lon, 
-              }
-          })
-          this.props.getLocations('/locations/closest_by_lat_lon.json?lat=' + props.user.lat + ';lon=' + props.user.lon + ';send_all_within_distance=1;max_distance=5')
-      }
-      
-      if (!this.props.user.id && props.user.id) {
-          this.props.getFavoriteLocations(props.user.id)
-      }
+        this.props.getCurrentLocation()
+        this.props.getLocationTypes('/location_types.json')
+        this.props.getMachines('/machines.json')
+        this.props.getOperators('/operators.json')
+    }
 
-      if (locationId !== this.props.query.locationId) {
-          this.props.navigation.navigate('LocationDetails', {id: locationId.toString(), locationName})
-      }
+    UNSAFE_componentWillReceiveProps(props) {
+        const { machineId, locationType, numMachines, selectedOperator, curLat, curLon, latDelta, lonDelta, locationId, locationName } = props.query
+        if (!this.props.user.lat && props.user.lat) {
+            this.setState({
+                region: {
+                    ...this.state.region,
+                    latitude: props.user.lat, 
+                    longitude: props.user.lon, 
+                }
+            })
+            this.props.getLocations('/locations/closest_by_lat_lon.json?lat=' + props.user.lat + ';lon=' + props.user.lon + ';send_all_within_distance=1;max_distance=5')
+        }
+        
+        if (!this.props.user.id && props.user.id) {
+            this.props.getFavoriteLocations(props.user.id)
+        }
 
-      if (props.locations.mapLocations !== this.props.locations.mapLocations) {
-          this.setState({ locations: props.locations.mapLocations })
-      }
+        if (locationId !== this.props.query.locationId) {
+            this.props.navigation.navigate('LocationDetails', {id: locationId.toString(), locationName})
+        }
 
-      if (curLat !== this.props.query.curLat || curLon !== this.props.query.curLon ) {
-          this.setState({
-              region: {
-                  latitude: curLat,
-                  longitude: curLon,
-                  latitudeDelta: latDelta,
-                  longitudeDelta: lonDelta,
+        if (props.locations.mapLocations !== this.props.locations.mapLocations) {
+            this.setState({ locations: props.locations.mapLocations })
+        }
 
-              }
-          })
-      }
+        if (curLat !== this.props.query.curLat || curLon !== this.props.query.curLon ) {
+            this.setState({
+                region: {
+                    latitude: curLat,
+                    longitude: curLon,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: lonDelta,
 
-      if (machineId !== this.props.query.machineId || locationType !== this.props.query.locationType || numMachines !== this.props.query.numMachines || selectedOperator !== this.props.query.selectedOperator) {
-          const machine = machineId ? `by_machine_id=${machineId};` : ''
-          const byLocationType = locationType ? `by_type_id=${locationType};` : ''
-          const byNumMachines = numMachines ? `by_at_least_n_machines_type=${numMachines};` : ''
-          const byOperator = selectedOperator ? `by_operator_id=${selectedOperator};` : ''
-          this.props.getLocations(`/locations/closest_by_lat_lon.json?lat=${this.state.region.latitude};lon=${this.state.region.longitude};${machine}${byLocationType}${byNumMachines}${byOperator}max_distance=5;send_all_within_distance=1`, true)
-      }
+                }
+            })
+        }
 
-  }
+        if (machineId !== this.props.query.machineId || locationType !== this.props.query.locationType || numMachines !== this.props.query.numMachines || selectedOperator !== this.props.query.selectedOperator) {
+            const machine = machineId ? `by_machine_id=${machineId};` : ''
+            const byLocationType = locationType ? `by_type_id=${locationType};` : ''
+            const byNumMachines = numMachines ? `by_at_least_n_machines_type=${numMachines};` : ''
+            const byOperator = selectedOperator ? `by_operator_id=${selectedOperator};` : ''
+            this.props.getLocations(`/locations/closest_by_lat_lon.json?lat=${this.state.region.latitude};lon=${this.state.region.longitude};${machine}${byLocationType}${byNumMachines}${byOperator}max_distance=5;send_all_within_distance=1`, true)
+        }
 
-  render(){
-      const { isFetchingLocations, isRefetchingLocations } = this.props.locations
-      const { machineId = false, locationType = false, numMachines = false, selectedOperator = false } = this.props.query
-      const filterApplied = machineId || locationType || numMachines || selectedOperator ? true : false
+    }
 
-      if (!this.state.authCheck) {
-          return (
-              <View style={{flex: 1, padding: 20}}>
-                  <ActivityIndicator/>
-              </View>
-          )
-      } 
+    render(){
+        const { isFetchingLocations, isRefetchingLocations } = this.props.locations
+        const { machineId = false, locationType = false, numMachines = false, selectedOperator = false } = this.props.query
+        const filterApplied = machineId || locationType || numMachines || selectedOperator ? true : false
 
-      if (isFetchingLocations || !this.state.region.latitude) {
-          return(
-              <View style={{flex: 1, padding: 20}}>
-                  <ActivityIndicator/>
-              </View>
-          )
-      }
+        if (!this.state.authCheck) {
+            return (
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        } 
 
-      return(
-          <View style={{flex: 1}}>
-              {filterApplied ? 
-                  <View style={s.filter}>
-                      <Button 
-                          title={'Clear Filter'} 
-                          onPress={() => this.props.clearFilters()}
-                          clear={true}
-                          containerStyle={{width:100,position:'absolute',right:0}}
-                          titleStyle={{fontSize:14,color:"#F53240"}}
-                      />
-                  </View> : 
-                  null
-              }
-              {isRefetchingLocations ? <Text style={s.loading}>Loading...</Text> : null}
-              <View style ={{flex:1, position: 'absolute',left: 0, top: 0, bottom: 0, right: 0}}>
-                  <MapView
-                      ref={this.mapRef}
-                      region={this.state.region}
-                      provider={ PROVIDER_GOOGLE }
-                      style={s.map}
-                      onRegionChange={this.onRegionChange}
-                  >
-                      {this.state.locations ? this.state.locations.map(l => (
-                          <MapView.Marker
-                              coordinate={{
-                                  latitude: Number(l.lat), 
-                                  longitude: Number(l.lon), 
-                                  latitudeDelta: this.state.region.latitudeDelta, 
-                                  longitudeDelta: this.state.region.longitudeDelta,
-                              }}
-                              title={l.name}
-                              key={l.id}
-                          >
-                              <MapView.Callout onPress={() => this.props.navigation.navigate('LocationDetails', {id: l.id, locationName: l.name})}>
-                                  <View style={s.calloutStyle}>
-                                      <Text>{l.name}</Text>
-                                      <Ionicons style={s.iconStyle} name="ios-arrow-dropright"/>
-                                  </View>
-                              </MapView.Callout>
-                          </MapView.Marker>
-                      )) : null}
-                  </MapView>
-              </View>
-          </View>
-      )
-  }
+        if (isFetchingLocations || !this.state.region.latitude) {
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
+
+        return(
+            <View style={{flex: 1}}>
+                <View style={s.filter}>
+                    <Icon
+                        raised
+                        name='location-arrow'
+                        type='font-awesome'
+                        color='#4b5862'
+                        size={20}
+                        onPress={() => {
+                            this.props.getLocations('/locations/closest_by_lat_lon.json?lat=' + this.props.user.lat + ';lon=' + this.props.user.lon + ';send_all_within_distance=1;max_distance=5', true)
+                            this.props.updateCoordinates(this.props.user.lat, this.props.user.lon)
+                        }}
+                    />
+                </View>                
+                {filterApplied ? 
+                    <View style={s.filter}>
+                        <Button 
+                            title={'Clear Filter'} 
+                            onPress={() => this.props.clearFilters()}
+                            clear={true}
+                            containerStyle={{width:100,position:'absolute',right:0}}
+                            titleStyle={{fontSize:14,color:"#F53240"}}
+                        />
+                    </View> : null                                    
+                }
+                {isRefetchingLocations ? <Text style={s.loading}>Loading...</Text> : null}
+                <View style ={{flex:1, position: 'absolute',left: 0, top: 0, bottom: 0, right: 0}}>
+                    <MapView
+                        ref={this.mapRef}
+                        region={this.state.region}
+                        provider={ PROVIDER_GOOGLE }
+                        style={s.map}
+                        onRegionChange={this.onRegionChange}
+                    >
+                        {this.state.locations ? this.state.locations.map(l => (
+                            <MapView.Marker
+                                coordinate={{
+                                    latitude: Number(l.lat), 
+                                    longitude: Number(l.lon), 
+                                    latitudeDelta: this.state.region.latitudeDelta, 
+                                    longitudeDelta: this.state.region.longitudeDelta,
+                                }}
+                                title={l.name}
+                                key={l.id}
+                            >
+                                <MapView.Callout onPress={() => this.props.navigation.navigate('LocationDetails', {id: l.id, locationName: l.name})}>
+                                    <View style={s.calloutStyle}>
+                                        <Text>{l.name}</Text>
+                                        <Ionicons style={s.iconStyle} name="ios-arrow-dropright"/>
+                                    </View>
+                                </MapView.Callout>
+                            </MapView.Marker>
+                        )) : null}
+                    </MapView>
+                </View>
+            </View>
+        )
+    }
 }
 
 const s = StyleSheet.create({
@@ -257,11 +272,18 @@ const s = StyleSheet.create({
     loading: {
         textAlign: 'center',
         zIndex: 10, 
-        fontSize: 18,
+        fontSize: 16,
     },
     filter: {
         zIndex: 10, 
-    }
+    },
+    location: {
+        backgroundColor:"#f5fbff",
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        elevation: 0,
+    },
 })
 
 Map.propTypes = {
