29be81bef9bcb5db85d8462ff338201ae97c2c09
ryantg <gratzer@posteo.org>
2019-02-25 21:46:52 -0800

    pbmtext component

---
+++ app/components/PbmText.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, Text } from 'react-native'
+
+class PbmText extends Component {
+    render() {
+      return (
+        <Text style={[s.text, this.props.style]}>
+          {this.props.children}
+        </Text>
+      )
+    }
+  }
+
+PbmText.propTypes = {
+    children: PropTypes.string,
+}
+
+const s = StyleSheet.create({
+    text: {
+        color: "#000e18"
+    },
+})
+
+export default PbmText
