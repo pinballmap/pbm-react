88d161a04a04f0fdd3b98c8891704f1e40b8bbbf
ryantg <gratzer@posteo.org>
2019-03-01 08:35:10 -0800

    user profile not logged in link goes to Login not Signup

--- app/components/NotLoggedIn.js
+++ app/components/NotLoggedIn.js
@@ -6,7 +6,7 @@ import { PbmButton } from './'
 class NotLoggedIn extends Component {
     render(){
         return(
-            <View>
+            <View style={{backgroundColor:'#f5fbff'}}>
                 <Text style={s.pageTitle}>{this.props.title}</Text>
                 <Text style={s.hiya}>{this.props.text}</Text>
                 <PbmButton
