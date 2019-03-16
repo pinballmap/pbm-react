60c0899891111c1a44d2fa20a41cf89096c1785b
Beth Poore <bpoore@discogsinc.com>
2019-03-16 14:33:02 -0700

    Starting on recent activity screen

--- app/screens/RecentActivity.js
+++ app/screens/RecentActivity.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Text, View } from 'react-native'
+import { ListItem } from 'react-native-elements'
 import { HeaderBackButton } from 'react-navigation'
+import { getData } from '../config/request'
+const moment = require('moment')
 
 class RecentActivity extends Component {
+    state = {
+        fetchingRecentActivity: this.props.user.locationTrackingServicesEnabled ? true : false,
+        recentActivity: [],
+    }
+    
     static navigationOptions = ({ navigation }) => {
         return {
             headerLeft: <HeaderBackButton tintColor="#4b5862" onPress={() => navigation.goBack(null)} />,
@@ -12,11 +22,62 @@ class RecentActivity extends Component {
             },
             headerTintColor: '#4b5862'
         }
-    };
+    }
 
-  render(){
-      return(<View><Text>RecentActivity Screen</Text></View>)
-  }
+    componentDidMount() {
+        const { locationTrackingServicesEnabled, lat, lon } = this.props.user
+        //The listener will refetch recent activity data every time this screen is navigated to
+        this.willFocusListener = this.props.navigation.addListener('willFocus', () => {
+            if (locationTrackingServicesEnabled) {
+                getData(`/user_submissions/list_within_range.json?lat=${lat};lon=${lon}`)
+                    .then(data => {
+                        console.log(data)
+                        this.setState({
+                            fetchingRecentActivity: false,
+                            recentActivity: data.user_submissions,
+                        })
+                    })
+            }
+        })
+    }
+      
+
+    render(){
+        const { recentActivity } = this.state
+        const { locationTrackingServicesEnabled } = this.props.user
+
+        return(
+            <View>
+                {!locationTrackingServicesEnabled ? 
+                    <Text>Enable location tracking to view recent nearby activity</Text> :
+                    recentActivity.length === 0 ?
+                        <Text>No recent activity</Text> :
+                        recentActivity.map(activity => {
+                            return (
+                                <View key={activity.id}>
+                                    <View
+                                        style={{
+                                            borderBottomColor: '#D3ECFF',
+                                            borderBottomWidth: 1,
+                                        }}
+                                    />
+                                    <ListItem
+                                        title={activity.submission}
+                                        subtitle={`Updated At: ${moment(activity.updated_at).format('LL')}`}
+                                    />
+                                </View>
+                            )})
+                }
+            </View>
+        )
+    }
 }
 
-export default RecentActivity
+RecentActivity.propTypes = {
+    user: PropTypes.object,
+    navigation: PropTypes.object,
+}
+
+const mapStateToProps = ({ user }) => ({ user })
+export default connect(mapStateToProps, null)(RecentActivity)
+
