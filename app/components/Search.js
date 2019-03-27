c4f33137b25a38be9a4b6e6b870291d354bc0337
Beth Poore <bpoore@discogsinc.com>
2019-03-27 16:51:50 -0700

    Add SafeAreaView to Search modal

--- app/components/Search.js
+++ app/components/Search.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 //https://www.peterbe.com/plog/how-to-throttle-and-debounce-an-autocomplete-input-in-react
 import { debounce } from 'throttle-debounce'
-import { Modal, Text, TouchableOpacity, View, Dimensions, StyleSheet, ScrollView } from 'react-native'
+import { Modal, Text, TouchableOpacity, View, Dimensions, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
 import { Input, ListItem } from 'react-native-elements'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
@@ -19,7 +19,8 @@ class Search extends Component {
             q: '',
             foundLocations: [],
             foundCities: [],
-            searchModalVisible: false
+            searchModalVisible: false,
+            showNoResults: false,
         }
 
         this.autocompleteSearchDebounced = debounce(500, this.autocompleteSearch)
@@ -27,7 +28,7 @@ class Search extends Component {
     }
 
     changeQuery = q => {
-        this.setState({ q }, () => {
+        this.setState({ q, showNoResults: false }, () => {
             this.autocompleteSearchDebounced(this.state.q)
         })
     }
@@ -44,13 +45,13 @@ class Search extends Component {
             const foundLocations = await getData(`/locations/autocomplete?name=${query}`)
             let foundCities = await getData(`/locations/autocomplete_city.json?name=${query}`)
             if (query === this.waitingFor) {
-                this.setState({ foundLocations, foundCities })
+                this.setState({ foundLocations, foundCities, showNoResults: true })
             }
         }
     }
 
     render(){
-        const { q, foundLocations = [], foundCities = [], searchModalVisible } = this.state
+        const { q, foundLocations = [], foundCities = [], searchModalVisible, showNoResults } = this.state
 
         return(
             <View>
@@ -59,68 +60,72 @@ class Search extends Component {
                     visible={searchModalVisible}
                     onRequestClose={()=>{}}
                 >
-                    <View style={{flex:1,backgroundColor:'rgba(255,255,255,1.0)'}}>
-                        <View style={{paddingTop: 20, display: 'flex', flexDirection: 'row'}}>
-                            <MaterialIcons 
-                                onPress={() => {
-                                    this.setState({searchModalVisible: false})
-                                    this.changeQuery('')
-                                }}
-                                name='clear' 
-                                size={30} 
-                                style={{color:'#6a7d8a',marginLeft:5,marginRight:10,marginTop:6}}
-                            />
-                            <Input
-                                placeholder='City, Address, Location'
-                                leftIcon={<MaterialIcons name='search' size={25} color="#6a7d8a" style={{marginLeft:-10,marginRight:-8}}/>}
-                                rightIcon={q ? <MaterialCommunityIcons name='close-circle' size={20} color="#97a5af" style={{marginRight:2}} onPress={() => this.changeQuery('')} /> : null}
-                                onChangeText={query => this.changeQuery(query)}
-                                value={q}
-                                containerStyle={{paddingTop:4}}
-                                inputContainerStyle={s.input}
-                                autoFocus
-                            />
+                    <SafeAreaView style={{flex: 1}}>
+                        <View style={{flex:1,backgroundColor:'rgba(255,255,255,1.0)'}}>
+                            <View style={{display: 'flex', flexDirection: 'row'}}>
+                                <MaterialIcons 
+                                    onPress={() => {
+                                        this.setState({searchModalVisible: false})
+                                        this.changeQuery('')
+                                    }}
+                                    name='clear' 
+                                    size={30} 
+                                    style={{color:'#6a7d8a',marginLeft:5,marginRight:10,marginTop:6}}
+                                />
+                                <Input
+                                    placeholder='City, Address, Location'
+                                    leftIcon={<MaterialIcons name='search' size={25} color="#6a7d8a" style={{marginLeft:-10,marginRight:-8}}/>}
+                                    rightIcon={q ? <MaterialCommunityIcons name='close-circle' size={20} color="#97a5af" style={{marginRight:2}} onPress={() => this.changeQuery('')} /> : null}
+                                    onChangeText={query => this.changeQuery(query)}
+                                    value={q}
+                                    containerStyle={{paddingTop:4}}
+                                    inputContainerStyle={s.input}
+                                    autoFocus
+                                />
+                            </View>
+                            <ScrollView style={{paddingTop: 3}}>
+                                {foundCities ? 
+                                    foundCities.map(location => 
+                                        (<TouchableOpacity 
+                                            key={location.value} 
+                                            onPress={() => {
+                                                this.props.getLocationsByCity(location.value, this.props.navigate)
+                                                this.changeQuery('')
+                                                this.setState({searchModalVisible: false})
+                                            }}>
+                                            <ListItem
+                                                title={location.value}
+                                                rightTitle={'City'}
+                                                rightTitleStyle={{fontStyle:'italic',color:'#97a5af'}}
+                                                titleStyle={{color:'#4b5862',marginBottom:-2,marginTop:-2}}
+                                                containerStyle={{borderBottomColor:'#97a5af',borderBottomWidth:1,backgroundColor:'#f2f4f5'}}
+                                            /> 
+                                        </TouchableOpacity>)
+                                    ) : null
+                                }
+                                {foundLocations ? 
+                                    foundLocations.map(location => 
+                                        (<TouchableOpacity 
+                                            key={location.id} 
+                                            onPress={() => {
+                                                this.props.navigate('LocationDetails', {id: location.id, locationName: location.label})
+                                                this.changeQuery('')
+                                                this.setState({searchModalVisible: false})
+                                            }}>
+                                            <ListItem
+                                                title={location.label}
+                                                titleStyle={{color:'#4b5862',marginBottom:-2,marginTop:-2}}
+                                                containerStyle={{borderBottomColor:'#97a5af',borderBottomWidth:1}}
+                                            /> 
+                                        </TouchableOpacity>)
+                                    ) : null
+                                }                        
+                                {foundLocations.length === 0 && foundCities.length === 0 && q !== '' && showNoResults ? 
+                                    <Text style={s.noResults}>No search results...</Text> : null 
+                                }
+                            </ScrollView>
                         </View>
-                        <ScrollView>
-                            {foundCities ? 
-                                foundCities.map(location => 
-                                    (<TouchableOpacity 
-                                        key={location.value} 
-                                        onPress={() => {
-                                            this.props.getLocationsByCity(location.value, this.props.navigate)
-                                            this.changeQuery('')
-                                            this.setState({searchModalVisible: false})
-                                        }}>
-                                        <ListItem
-                                            title={location.value}
-                                            rightTitle={'City'}
-                                            rightTitleStyle={{fontStyle:'italic',color:'#97a5af'}}
-                                            titleStyle={{color:'#4b5862',marginBottom:-2,marginTop:-2}}
-                                            containerStyle={{borderBottomColor:'#97a5af',borderBottomWidth:1,backgroundColor:'#f2f4f5'}}
-                                        /> 
-                                    </TouchableOpacity>)
-                                ) : null
-                            }
-                            {foundLocations ? 
-                                foundLocations.map(location => 
-                                    (<TouchableOpacity 
-                                        key={location.id} 
-                                        onPress={() => {
-                                            this.props.navigate('LocationDetails', {id: location.id, locationName: location.label})
-                                            this.changeQuery('')
-                                            this.setState({searchModalVisible: false})
-                                        }}>
-                                        <ListItem
-                                            title={location.label}
-                                            titleStyle={{color:'#4b5862',marginBottom:-2,marginTop:-2}}
-                                            containerStyle={{borderBottomColor:'#97a5af',borderBottomWidth:1}}
-                                        /> 
-                                    </TouchableOpacity>)
-                                ) : null
-                            }                        
-                            {foundLocations.length === 0 && foundCities.length === 0 && q !== '' ? <Text>No search results...</Text> : null }
-                        </ScrollView>
-                    </View>
+                    </SafeAreaView>
                 </Modal>
                 <TouchableOpacity onPress={() => this.setState({searchModalVisible: true})}>
                     <View style={s.searchMap}>
@@ -131,6 +136,10 @@ class Search extends Component {
             </View>
         )
     }
+}
+
+Search.propTypes = {
+    getLocationsByCity: PropTypes.func,
 }
 
 const s = StyleSheet.create({
@@ -157,6 +166,10 @@ const s = StyleSheet.create({
         flexDirection: 'row',
         paddingLeft:0
     },
+    noResults: {
+        paddingTop: 15,
+        paddingLeft: 20,
+    }
 })
 
 Search.propTypes = {
