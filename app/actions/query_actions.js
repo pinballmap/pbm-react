976713ef54c314954c2b6a82ff7f4add259f85fa
Beth Poore <bpoore@discogsinc.com>
2019-03-29 17:54:12 -0700

    Initial work moving filters to appState to allow using MachineFind screen

--- app/actions/query_actions.js
+++ app/actions/query_actions.js
@@ -1,22 +1,22 @@
 import {
     UPDATE_COORDINATES,
-    SET_FILTERS,
     CLEAR_FILTERS,
     SET_SELECTED_ACTIVITY_FILTER,
     CLEAR_ACTIVITY_FILTER,
+    SET_MACHINE_FILTER,
+    SET_NUM_MACHINES_FILTER,
+    SET_LOCATION_TYPE_FILTER,
+    SET_OPERATOR_FILTER,
 } from './types'
 
 export const updateCurrCoordindates = (lat, lon, latDelta = 0.1, lonDelta = 0.1) =>  ({ type: UPDATE_COORDINATES, lat, lon, latDelta, lonDelta }) 
 
-export const setFilters = (selectedMachine, selectedLocationType, numMachines = 0, selectedOperator) => ({
-    type: SET_FILTERS,
-    selectedMachine, 
-    selectedLocationType, 
-    numMachines, 
-    selectedOperator,
-})
-
 export const clearFilters = () => ({ type: CLEAR_FILTERS })
+
+export const setMachineFilter = (machine) => ({ type: SET_MACHINE_FILTER, machine })
+export const updateNumMachinesSelected = (numMachines) => ({ type: SET_NUM_MACHINES_FILTER, numMachines })
+export const selectedLocationTypeFilter = (locationType) => ({ type: SET_LOCATION_TYPE_FILTER, locationType })
+export const selectedOperatorTypeFilter = (selectedOperator) => ({ type: SET_OPERATOR_FILTER, selectedOperator })
 
 export const setSelectedActivityFilter = (selectedActivity) => ({
     type: SET_SELECTED_ACTIVITY_FILTER,
