7bddf27cff94c839677756c1435b3c97c82fed49
Beth Poore <bpoore@discogsinc.com>
2019-04-14 16:20:57 -0700

    Get android working

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp7ce55ff6c4c64590b9fbabb2f23d3568://10.0.0.37:19000";
 
 }
