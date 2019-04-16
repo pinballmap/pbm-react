ad82c21bd06160b13e63ce6c87db68d6b47d671c
Beth Poore <bpoore@discogsinc.com>
2019-04-15 18:28:39 -0700

    gitignore stuff

--- ios/Pods/Headers/Public/ExpoKit/EXScopedModuleRegistryAdapter.h
+++
@@ -1,9 +0,0 @@
-// Copyright © 2018 650 Industries. All rights reserved.
-
-#import <EXReactNativeAdapter/EXModuleRegistryAdapter.h>
-
-@interface EXScopedModuleRegistryAdapter : EXModuleRegistryAdapter
-
-- (NSArray<id<RCTBridgeModule>> *)extraModulesForParams:(NSDictionary *)params andExperience:(NSString *)experienceId withScopedModulesArray:(NSArray *)scopedModulesArray withKernelServices:(NSDictionary *)kernelServices;
-
-@end
