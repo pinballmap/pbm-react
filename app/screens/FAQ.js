df5c0ae666b161d373e2c94b8637a9e005834ffb
ryantg <gratzer@posteo.org>
2019-05-06 08:30:31 -0700

    Q about operator in FAQ

--- app/screens/FAQ.js
+++ app/screens/FAQ.js
@@ -45,8 +45,10 @@ class FAQ extends Component {
                             <Text style={s.text}>{`Click the menu icon in the lower right, and choose "Add Location". Then fill out the form! We moderate the submissions, so it will a few days for the location you submitted to be added to the map. The more accurate and thorough your submission, the quicker it will get added! Make sure to include at least one machine with your submission.`}</Text>
                             <Text style={s.bold}>{`How do I remove a machine from a location?`}</Text>
                             <Text style={s.text}>{`Click on the machine name, and then look for a "remove" button.`}</Text>
-                            <Text style={s.bold}>{`This location closed/no longer has machines.`}</Text>
+                            <Text style={s.bold}>{`This location closed/no longer has machines. What do I do - do I need to tell you?`}</Text>
                             <Text style={s.text}>{`Simply remove all the machines from it. Empty locations are periodically removed.`}</Text>
+                            <Text style={s.bold}>{`How do I get listed as an operator?`}</Text>
+        <Text style={s.text}>{<Text onPress={ () => this.props.navigation.navigate('Contact') } style={{textDecorationLine: 'underline'}}>{"Contact us"}</Text>}{ ` and we'll add you.`}</Text>
                             <Text style={s.bold}>{`What is your privacy policy?`}</Text>
                             <Text style={s.text}>{`We do not track or store user locations, nor store any personal information. We do not sell any user data. We do not use third-party analytics. This site is not monetized. We keep a log of map edits that users make.`}</Text>
                             <Text style={s.bold}>{`Have a question that we didn't cover here?`} <Text onPress={ () => this.props.navigation.navigate('Contact') } style={s.textLink}>{"Ask us!"}</Text></Text>
