68eb3034bd06fdaf0914d16744b13d1a82ffbb0c
metroshica <landonwo@gmail.com>
2019-06-05 13:01:36 -0600

    Remove unnecessary openMap import.

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Font } from 'expo'
-import openMap from 'react-native-open-maps';
 import { 
     ActivityIndicator,
     Platform,
