2371da45bc2b7910160897842a93b24f360993e6
bpoore <beth.poore@gmail.com>
2019-10-02 18:52:24 -0700

    use theme provider for different color modes

--- app/index.js
+++ app/index.js
@@ -1,11 +1,25 @@
 import { registerRootComponent } from 'expo'
-import { AppearanceProvider } from 'react-native-appearance'
+//import { AppearanceProvider } from 'react-native-appearance'
+import { Appearance } from 'react-native-appearance'
 import React, { Component } from 'react'
-import { StatusBar, Platform } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
+import { ThemeProvider } from 'react-native-elements'
 import { Provider } from 'react-redux'
 import { PbmStack } from './config/router'
 
 import store from './store'
+
+const theme = Appearance.getColorScheme()
+
+const dark = {
+    buttonColor: '#000',
+    textColor: '#FFF'
+}
+
+const standard = {
+    buttonColor: '#D3ECFF',
+    textColor: '#4b5862'
+}
 
 class App extends Component {
     componentDidMount() {
@@ -18,11 +32,11 @@ class App extends Component {
     }
     render() {
         return (
-            <AppearanceProvider>
+            <ThemeProvider theme={theme === 'dark' ? dark : standard}>
                 <Provider store={store}>
                     <PbmStack />
                 </Provider>
-            </AppearanceProvider>
+            </ThemeProvider>
         )
     }
 }
