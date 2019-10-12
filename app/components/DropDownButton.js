aeb7cb3b2c414883b1ff3c7d4787a200870a2e33
bpoore <beth.poore@gmail.com>
2019-10-12 15:07:11 -0700

    DropDownButton to functional + theme

--- app/components/DropDownButton.js
+++ app/components/DropDownButton.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet } from 'react-native'
-import { Button } from 'react-native-elements'
+import { Button, ThemeContext } from 'react-native-elements'
 import { Ionicons } from '@expo/vector-icons'
 
-class DropDownButton extends Component {
-    render(){
-        return(
-            <Button
-                title={this.props.title}
-                onPress={() => this.props.onPress()}
-                buttonStyle={s.dropdown}
-                titleStyle={{
-                    color:"#000e18",
-                    fontSize:16
-                }}
-                style={{borderRadius: 10}}
-                icon={<Ionicons name='md-arrow-dropdown' style={s.dropdownIcon} />}
-                iconRight
-                containerStyle={{marginTop:5,marginRight:10,marginLeft:10}}
-            />
-        )
-    }
+const DropDownButton = ({ title, onPress, }) => {
+    const { theme } = useContext(ThemeContext)
+
+    return(
+        <Button
+            title={title}
+            onPress={onPress}
+            buttonStyle={s(theme).dropdown}
+            titleStyle={s(theme).titleStyle}
+            style={{borderRadius: 10}}
+            icon={<Ionicons name='md-arrow-dropdown' style={s(theme).dropdownIcon} />}
+            iconRight
+            containerStyle={{marginTop:5,marginRight:10,marginLeft:10}}
+        />
+    )
 }
 
 DropDownButton.propTypes = {
@@ -32,7 +29,7 @@ DropDownButton.propTypes = {
     disabled: PropTypes.bool,
 }
 
-const s = StyleSheet.create({
+const s = theme => StyleSheet.create({
     dropdown: {
         backgroundColor:"#e0ebf2",
         width: '100%',
@@ -44,6 +41,10 @@ const s = StyleSheet.create({
     dropdownIcon: {
         color: "#4b5862",
         fontSize: 32
+    },
+    titleStyle: {
+        color:"#000e18",
+        fontSize:16
     }
 })
 
