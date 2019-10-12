85c31856e3f1dc44999e226c7b1a5bd398766380
bpoore <beth.poore@gmail.com>
2019-10-12 15:00:52 -0700

    Confirmation modal to functional component + theme

--- app/components/ConfirmationModal.js
+++ app/components/ConfirmationModal.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { 
     Modal,
     StyleSheet, 
     View, 
 } from 'react-native'
+import { ThemeContext } from 'react-native-elements'
 
-class ConfirmationModal extends Component {
-    render(){
-        return(
-            <Modal
-                animationType="slide"
-                transparent={true}
-                onRequestClose={()=>{}}
-                visible={this.props.visible}
-            >
-                <View style={s.modalBg}>
-                    <View style= {s.modal}>
-                        {this.props.children}
-                    </View>
+const ConfirmationModal = ({children, visible}) => {
+    const { theme } = useContext(ThemeContext)
+
+    return(
+        <Modal
+            animationType="slide"
+            transparent={true}
+            onRequestClose={()=>{}}
+            visible={visible}
+        >
+            <View style={s(theme).modalBg}>
+                <View style= {s(theme).modal}>
+                    {children}
                 </View>
-            </Modal>    
+            </View>
+        </Modal>    
 
-        )
-    }
+    )
 }
 
 ConfirmationModal.propTypes = {
@@ -31,7 +32,7 @@ ConfirmationModal.propTypes = {
     children: PropTypes.node,
 }
 
-const s = StyleSheet.create({
+const s = (theme) => StyleSheet.create({
     modalBg: {
         flex: 1,
         flexDirection: 'column',
@@ -40,7 +41,7 @@ const s = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0,0.5)'
     },
     modal: {
-        backgroundColor: '#ffffff',
+        backgroundColor: theme.modalBg,
         borderRadius: 15,
         width: '80%',
         paddingVertical: 15,
