fba8574899e10a848faebda35651e9bd63d2e96b
bpoore <beth.poore@gmail.com>
2019-10-18 10:46:40 -0700

    Drawer menu to function component

--- app/components/DrawerMenu.js
+++ app/components/DrawerMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useContext, useState } from 'react'
 import { connect } from 'react-redux' 
 import PropTypes from 'prop-types'
 import { DrawerNavigatorItems } from 'react-navigation-drawer'
@@ -9,63 +9,57 @@ import {
     View,
     StyleSheet
 } from 'react-native'
+import { ThemeContext } from 'react-native-elements'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { logout } from '../actions'
 import ConfirmationModal from './ConfirmationModal'
 import PbmButton from './PbmButton'
 import WarningButton from './WarningButton'
 
-class DrawerMenu extends Component {
-    state = {
-        modalVisible: false,
-    }
+const DrawerMenu = ({ loggedIn, logout, navigation, ...props }) => {
+    const { theme } = useContext(ThemeContext)
+    const s = getStyles(theme)
 
-    render(){
-        const { 
-            loggedIn,
-            logout, 
-            navigation,
-        } = this.props
+    const [modalVisible, setModalVisible] = useState(false)
 
-        return(
-            <View>
-                <ConfirmationModal visible={this.state.modalVisible} >
-                    <PbmButton
-                        title={"Yes! Log Me Out"}
-                        onPress={() => {
-                            this.setState({ modalVisible: false})
-                            logout()
-                            navigation.navigate('Login')
-                        }}
-                        accessibilityLabel="Logout"
-                    />
-                    <WarningButton
-                        title={"Stay Logged In"}
-                        onPress={() => this.setState({ modalVisible: false})}
-                        accessibilityLabel="Stay Logged In"
-                    />
-                </ConfirmationModal>
-                <View style={{marginTop: 50}}>
-                    <DrawerNavigatorItems 
-                        {...this.props} 
-                        onItemPress={(item) => navigation.navigate(item.route.key)}
-                    />
-                    {loggedIn ? 
-                        <TouchableOpacity style={s.container} onPress={() => this.setState({ modalVisible: true})}>
-                            <MaterialCommunityIcons name='logout' style={s.icon} />
-                            <Text style={s.text}>Logout</Text>
-                        </TouchableOpacity>                    
-                        : <TouchableOpacity style={s.container} onPress={() => navigation.navigate('Login')}>
-                            <MaterialCommunityIcons name='login' style={s.icon} />
-                            <Text style={s.text}>Log In</Text>
-                        </TouchableOpacity>
-                    }
-                </View>
+    return(
+        <View>
+            <ConfirmationModal visible={modalVisible} >
+                <PbmButton
+                    title={"Yes! Log Me Out"}
+                    onPress={() => {
+                        setModalVisible(false)
+                        logout()
+                        navigation.navigate('Login')
+                    }}
+                    accessibilityLabel="Logout"
+                />
+                <WarningButton
+                    title={"Stay Logged In"}
+                    onPress={() => setModalVisible(false)}
+                    accessibilityLabel="Stay Logged In"
+                />
+            </ConfirmationModal>
+            <View style={{marginTop: 50}}>
+                <DrawerNavigatorItems 
+                    {...props} 
+                    onItemPress={(item) => navigation.navigate(item.route.key)}
+                />
+                {loggedIn ? 
+                    <TouchableOpacity style={s.container} onPress={() => setModalVisible(true)}>
+                        <MaterialCommunityIcons name='logout' style={s.icon} />
+                        <Text style={s.text}>Logout</Text>
+                    </TouchableOpacity>                    
+                    : <TouchableOpacity style={s.container} onPress={() => navigation.navigate('Login')}>
+                        <MaterialCommunityIcons name='login' style={s.icon} />
+                        <Text style={s.text}>Log In</Text>
+                    </TouchableOpacity>
+                }
             </View>
-        )
-    }
+        </View>
+    )
 }
-const s = StyleSheet.create({
+const getStyles = theme => StyleSheet.create({
     container: {
         marginTop: 8,
         height: 55,
@@ -85,6 +79,7 @@ const s = StyleSheet.create({
         top: 4
     },
 })
+
 DrawerMenu.propTypes = {
     loggedIn: PropTypes.bool,
     logout: PropTypes.func,
