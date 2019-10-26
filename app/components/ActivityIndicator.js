79b2469a29c3e3994c3029fbb621570e1c5f9307
bpoore <beth.poore@gmail.com>
2019-10-25 19:18:56 -0700

    Refactor profile to have theme. Themed ActivityIndicator component

---
+++ app/components/ActivityIndicator.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { 
+    ActivityIndicator,
+    StyleSheet, 
+    View,
+} from 'react-native'
+import { ThemeContext } from 'react-native-elements'
+
+const styledActivityIndicator = () => {
+    const { theme } = useContext(ThemeContext)
+    const s = getStyles(theme)
+
+    return (
+        <View style={s.container}>
+            <ActivityIndicator />
+        </View>
+    )
+}
+
+const getStyles = (theme) => StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 20,
+        backgroundColor:theme.backgroundColor,
+    },
+})
+
+export default styledActivityIndicator
