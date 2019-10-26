c3a585c5e2e63a0a7721fd66b78900eb62a388a1
bpoore <beth.poore@gmail.com>
2019-10-26 16:45:51 -0700

    ActivityIndicator component on Blog and Podcast screens

--- app/screens/Podcast.js
+++ app/screens/Podcast.js
@@ -1,26 +1,21 @@
-import React, { Fragment, useContext, useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import {
-    ActivityIndicator,
-    StyleSheet, 
     View
 } from 'react-native'
 import { WebView } from 'react-native-webview'
-import { ThemeContext } from 'react-native-elements'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { HeaderBackButton } from '../components'
+import { 
+    ActivityIndicator,
+    HeaderBackButton, 
+} from '../components'
 
 const Podcast = () => {
     const [loading, setLoading] = useState(true)
-    const { theme } = useContext(ThemeContext)
 
     return(
         <Fragment>
-            {loading && (
-                <View style={{height: '100%', padding: 30, backgroundColor:theme.backgroundColor}}>
-                    <ActivityIndicator/>
-                </View>
-            )}
+            {loading && <ActivityIndicator/>}
             <WebView
                 onLoad={() => setLoading(false)}
                 style={{ flex: 1 }}
@@ -33,17 +28,10 @@ const Podcast = () => {
 
 Podcast.navigationOptions = ({ navigation }) => ({
     drawerLabel: 'Podcast',
-    drawerIcon: () => <MaterialCommunityIcons name='radio-tower' style={[s.drawerIcon]} />, 
+    drawerIcon: () => <MaterialCommunityIcons name='radio-tower' style={{fontSize: 24,color: '#6a7d8a'}} />, 
     headerLeft: <HeaderBackButton navigation={navigation}/>,
     title: 'Podcast',
     headerRight:<View style={{padding:6}}></View>,
-})
-
-const s = StyleSheet.create({
-    drawerIcon: {
-        fontSize: 24,
-        color: '#6a7d8a'
-    },
 })
 
 Podcast.propTypes = {
