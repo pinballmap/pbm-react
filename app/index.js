dd8cd57685456adbf900174e9258389819a674f7
bpoore <beth.poore@gmail.com>
2020-12-06 18:37:52 -0800

    convert to functional component

--- app/index.js
+++ app/index.js
@@ -1,64 +1,47 @@
 import { registerRootComponent } from 'expo'
-import React, { Component } from 'react'
-import { Appearance, Platform } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Appearance } from 'react-native'
 import { retrieveItem } from './config/utils'
 import { ThemeContext } from './theme-context'
 import { Provider } from 'react-redux'
 import { PbmStack } from './config/router'
 import { dark, standard } from './utils/themes'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
 
 import store from './store'
 
 const defaultTheme = Appearance.getColorScheme()
 
-class App extends Component {
-    constructor(props) {
-        super(props)
+const App = () => {
+    const [selectedTheme, setSelectedTheme] = useState(defaultTheme === 'dark' ? 'dark' : '')
 
-        this.toggleDefaultTheme = () => {
-            defaultTheme !== 'dark' && this.setState(state => ({
-                selectedTheme: state.selectedTheme === 'dark' ? '' : 'dark'
-            }))
-        }
-
-        this.toggleDarkTheme = () => {
-            defaultTheme === 'dark' && this.setState(state => ({
-                selectedTheme: state.selectedTheme === 'dark' ? '' : 'dark'
-            }))
-        }
-
-        this.state={ selectedTheme: defaultTheme }
-    }
-
-    componentDidMount() {
-
+    useEffect(() => {
         retrieveItem('defaultThemeOverride')
-            .then(defaultThemeOverride => defaultTheme !== 'dark' && defaultThemeOverride && this.setState({selectedTheme: 'dark'}))
+            .then(defaultThemeOverride => defaultTheme !== 'dark' && defaultThemeOverride && setSelectedTheme('dark'))
 
         retrieveItem('darkThemeOverride')
-            .then(darkThemeOverride => defaultTheme === 'dark' && darkThemeOverride && this.setState({selectedTheme: ''}))
-    }
+            .then(darkThemeOverride => defaultTheme === 'dark' && darkThemeOverride && setSelectedTheme(''))
 
-    render() {
-        const { selectedTheme } = this.state
+    }, [])
 
-        return (
-            <SafeAreaProvider>
-                <ThemeContext.Provider value={{
-                    toggleDefaultTheme: this.toggleDefaultTheme,
-                    toggleDarkTheme: this.toggleDarkTheme,
-                    theme: selectedTheme === 'dark' ? dark : standard
-                }}>
-                    <Provider store={store}>
-                        <PbmStack theme={selectedTheme === 'dark' ? 'dark' : 'light'} />
-                    </Provider>
-                </ThemeContext.Provider>
-                <StatusBar style={selectedTheme === 'dark' ? 'light' : 'dark'} translucent={true} />
-            </SafeAreaProvider>
-        )
-    }
+    const toggleDefaultTheme = () => defaultTheme !== 'dark' && setSelectedTheme(selectedTheme === 'dark' ? '' : 'dark')
+    const toggleDarkTheme = () => defaultTheme === 'dark' && setSelectedTheme(selectedTheme === 'dark' ? '' : 'dark')
+
+    return (
+        <SafeAreaProvider>
+            <ThemeContext.Provider value={{
+                toggleDefaultTheme,
+                toggleDarkTheme,
+                theme: selectedTheme === 'dark' ? dark : standard
+            }}>
+                <Provider store={store}>
+                    <PbmStack theme={selectedTheme === 'dark' ? 'dark' : 'light'} />
+                </Provider>
+            </ThemeContext.Provider>
+            <StatusBar style={selectedTheme === 'dark' ? 'light' : 'dark'} translucent={true} />
+        </SafeAreaProvider>
+    )
 }
 
 export default registerRootComponent(App)
