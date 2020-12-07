b0cf71c0f3c7536229ac31d550b68c1fee34e070
bpoore <beth.poore@gmail.com>
2020-12-06 17:31:02 -0800

    move to expo status bar

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import { registerRootComponent } from 'expo'
 import React, { Component } from 'react'
-import { Appearance, Platform, StatusBar } from 'react-native'
+import { Appearance, Platform } from 'react-native'
 import { retrieveItem } from './config/utils'
 import { ThemeContext } from './theme-context'
 import { Provider } from 'react-redux'
 import { PbmStack } from './config/router'
 import { dark, standard } from './utils/themes'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
 
 import store from './store'
 
@@ -26,18 +27,12 @@ class App extends Component {
             defaultTheme === 'dark' && this.setState(state => ({
                 selectedTheme: state.selectedTheme === 'dark' ? '' : 'dark'
             }))
-        } 
-        
+        }
+
         this.state={ selectedTheme: defaultTheme }
     }
 
     componentDidMount() {
-        StatusBar.setBarStyle('dark-content')
-
-        if (Platform.OS === 'android') {
-            StatusBar.setTranslucent(true)
-            StatusBar.setBackgroundColor('transparent')
-        }
 
         retrieveItem('defaultThemeOverride')
             .then(defaultThemeOverride => defaultTheme !== 'dark' && defaultThemeOverride && this.setState({selectedTheme: 'dark'}))
@@ -45,21 +40,22 @@ class App extends Component {
         retrieveItem('darkThemeOverride')
             .then(darkThemeOverride => defaultTheme === 'dark' && darkThemeOverride && this.setState({selectedTheme: ''}))
     }
- 
+
     render() {
         const { selectedTheme } = this.state
-     
+
         return (
             <SafeAreaProvider>
                 <ThemeContext.Provider value={{
-                    toggleDefaultTheme: this.toggleDefaultTheme, 
-                    toggleDarkTheme: this.toggleDarkTheme, 
-                    theme: selectedTheme === 'dark' ? dark : standard 
+                    toggleDefaultTheme: this.toggleDefaultTheme,
+                    toggleDarkTheme: this.toggleDarkTheme,
+                    theme: selectedTheme === 'dark' ? dark : standard
                 }}>
                     <Provider store={store}>
                         <PbmStack theme={selectedTheme === 'dark' ? 'dark' : 'light'} />
                     </Provider>
                 </ThemeContext.Provider>
+                <StatusBar style={selectedTheme === 'dark' ? 'light' : 'dark'} translucent={true} />
             </SafeAreaProvider>
         )
     }
