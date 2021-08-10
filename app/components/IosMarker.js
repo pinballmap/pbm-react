03c1cc400c01f662e298e8c15ed2dcd64dddeb3e
ryantg <gratzer@fastmail.com>
2021-08-09 20:06:48 -0700

    Reduce font size and border width on marker dot

--- app/components/IosMarker.js
+++ app/components/IosMarker.js
@@ -6,16 +6,16 @@ import PropTypes from "prop-types"
 const IosMarker = ({numMachines}) => {
     let dotFontMargin, dotWidthHeight
     if (numMachines < 10) {
-        dotFontMargin = 0
+        dotFontMargin = 3
         dotWidthHeight = 32
     } else if (numMachines < 20) {
-        dotFontMargin = 2
+        dotFontMargin = 5
         dotWidthHeight = 36
     } else if (numMachines < 100) {
-        dotFontMargin = 4
+        dotFontMargin = 7
         dotWidthHeight = 40
     } else {
-        dotFontMargin = 7
+        dotFontMargin = 10
         dotWidthHeight = 46
     }
     return (
@@ -23,17 +23,16 @@ const IosMarker = ({numMachines}) => {
             width: dotWidthHeight,
             height: dotWidthHeight,
             borderRadius: dotWidthHeight / 2,
-            borderWidth: 3,
+            borderWidth: 2,
             borderColor: '#d2e5fa',
-            backgroundColor: '#78b6fb',
-            elevation: 1,
+            backgroundColor: '#78b6fb'
         }}>
             <Text style={{
                 color: 'white',
-                fontWeight:'bold',
-                textAlign:'center',
-                fontSize: 20,
-                marginTop:dotFontMargin}}>
+                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 18,
+                marginTop: dotFontMargin}}>
                 {numMachines}
             </Text>
         </View>
