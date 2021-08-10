0d1760f7aee606114926ce020ee973599c60e248
bpoore <beth.poore@gmail.com>
2021-08-10 10:02:47 -0700

    only need single height calculation

--- app/components/IosHeartMarker.js
+++ app/components/IosHeartMarker.js
@@ -5,26 +5,22 @@ import PropTypes from "prop-types"
 import markerDotHeart from '../assets/images/markerdot-heart.png'
 
 const IosHeartMarker = ({numMachines}) => {
-    let dotFontMargin, dotWidth, dotHeight
+    let dotFontMargin, dotWidth
     if (numMachines < 10) {
         dotFontMargin = 4
         dotWidth = 36
-        dotHeight = dotWidth * .875
     } else if (numMachines < 20) {
         dotFontMargin = 4
         dotWidth = 40
-        dotHeight = dotWidth * .875
     } else if (numMachines < 100) {
         dotFontMargin = 7
         dotWidth = 46
-        dotHeight = dotWidth * .875
     } else {
         dotFontMargin = 8
         dotWidth = 54
-        dotHeight = dotWidth * .875
     }
     return (
-        <ImageBackground style={{width: dotWidth, height: dotHeight}}source={markerDotHeart} >
+        <ImageBackground style={{width: dotWidth, height: dotWidth * .875}} source={markerDotHeart} >
             <Text style={{
                 color: 'white',
                 fontWeight: 'bold',
