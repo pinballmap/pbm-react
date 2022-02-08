fa53884ab160ab36680703a980f1e9b92c163018
ryantg <gratzer@fastmail.com>
2022-02-07 17:15:23 -0800

    5.2.10 release info

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
             bundleIdentifier: "net.isaacruiz.ppm",
             userInterfaceStyle: "automatic",
             icon: "app/assets/images/ios-icon.png",
-            buildNumber: "104",
+            buildNumber: "105",
             supportsTablet: true,
             infoPlist: {
                 NSLocationWhenInUseUsageDescription: "Allow access to device location to show nearby places with pinball machines"
@@ -63,7 +63,7 @@ export default {
                 "WRITE_EXTERNAL_STORAGE",
                 "com.google.android.providers.gsf.permission.READ_GSERVICES"
             ],
-            versionCode: 77,
+            versionCode: 78,
             adaptiveIcon: {
                 backgroundColor: "#ebecff",
                 foregroundImage: "app/assets/images/adaptive-foreground.png"
