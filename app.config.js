7ebafc6aa37e41960f579e6e213babb0661bd7e2
RyanTG <gratzer@fastmail.com>
2023-03-27 22:04:55 -0700

    remove authToken from config

--- app.config.js
+++ app.config.js
@@ -38,7 +38,6 @@ export default {
           config: {
             organization: "pinball-map",
             project: "react-native",
-            authToken: process.env.SENTRY_AUTH_TOKEN,
           },
         },
       ],
@@ -47,7 +46,7 @@ export default {
       bundleIdentifier: "net.isaacruiz.ppm",
       userInterfaceStyle: "automatic",
       icon: "app/assets/images/ios-icon.png",
-      buildNumber: "129",
+      buildNumber: "130",
       supportsTablet: true,
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
@@ -79,7 +78,7 @@ export default {
         "WRITE_EXTERNAL_STORAGE",
         "com.google.android.providers.gsf.permission.READ_GSERVICES",
       ],
-      versionCode: 105,
+      versionCode: 106,
       adaptiveIcon: {
         backgroundColor: "#ebecff",
         foregroundImage: "app/assets/images/adaptive-foreground.png",
