a6da02dfe33f814828f46d4cb3c2194987dd0117
RyanTG <gratzer@fastmail.com>
2023-04-10 19:22:20 -0700

    5.2.24 release

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default {
     runtimeVersion: {
       policy: "sdkVersion",
     },
-    version: "5.2.23",
+    version: "5.2.24",
     slug: "pbm-app",
     owner: "pinballmap",
     name: "Pinball Map",
@@ -46,7 +46,7 @@ export default {
       bundleIdentifier: "net.isaacruiz.ppm",
       userInterfaceStyle: "automatic",
       icon: "app/assets/images/ios-icon.png",
-      buildNumber: "131",
+      buildNumber: "132",
       supportsTablet: true,
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
@@ -78,7 +78,7 @@ export default {
         "WRITE_EXTERNAL_STORAGE",
         "com.google.android.providers.gsf.permission.READ_GSERVICES",
       ],
-      versionCode: 110,
+      versionCode: 111,
       adaptiveIcon: {
         backgroundColor: "#ebecff",
         foregroundImage: "app/assets/images/adaptive-foreground.png",
