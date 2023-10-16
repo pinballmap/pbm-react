f62d68c5fa680474d931a4223c3db872f2222789
RyanTG <gratzer@fastmail.com>
2023-10-15 21:43:14 -0700

    upgrade to rnmapbox 10.0.15

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ export default {
     runtimeVersion: {
       policy: "sdkVersion",
     },
-    version: "5.3.4",
+    version: "5.3.5",
     slug: "pbm-app",
     owner: "pinballmap",
     name: "Pinball Map",
@@ -54,7 +54,7 @@ export default {
       bundleIdentifier: "net.isaacruiz.ppm",
       userInterfaceStyle: "automatic",
       icon: "app/assets/images/ios-icon.png",
-      buildNumber: "157",
+      buildNumber: "158",
       supportsTablet: true,
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
