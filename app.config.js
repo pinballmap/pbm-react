3f233a08ee777250155699552570e6ef24cd6c4f
RyanTG <gratzer@fastmail.com>
2023-12-31 17:16:41 -0800

    android build number

--- app.config.js
+++ app.config.js
@@ -86,7 +86,7 @@ export default {
         "WRITE_EXTERNAL_STORAGE",
         "com.google.android.providers.gsf.permission.READ_GSERVICES",
       ],
-      versionCode: 142,
+      versionCode: 143,
       adaptiveIcon: {
         backgroundColor: "#ebecff",
         foregroundImage: "app/assets/images/adaptive-foreground.png",
