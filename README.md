95fd0306ae430a070e69f404a16a8b02b7f0fa8b
RyanTG <gratzer@fastmail.com>
2024-02-22 18:09:07 -0800

    Prepare for 5.3.12 release

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ This repo is the codebase for the [Pinball Map app](https://www.pinballmap.com/a
 
 Release dates are approximate, given that review times vary between the App Store and Play Store.
 
+### 5.3.12
+
+February 23, 2023
+
+- Fixed minor bug on About page.
+- A couple small design tweaks and package updates.
+
 ### 5.3.11
 
 February 8, 2023
--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ export default {
     runtimeVersion: {
       policy: "sdkVersion",
     },
-    version: "5.3.11",
+    version: "5.3.12",
     slug: "pbm-app",
     owner: "pinballmap",
     name: "Pinball Map",
@@ -65,7 +65,7 @@ export default {
       bundleIdentifier: "net.isaacruiz.ppm",
       userInterfaceStyle: "automatic",
       icon: "app/assets/images/ios-icon.png",
-      buildNumber: "175",
+      buildNumber: "176",
       supportsTablet: true,
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
@@ -97,7 +97,7 @@ export default {
         "WRITE_EXTERNAL_STORAGE",
         "com.google.android.providers.gsf.permission.READ_GSERVICES",
       ],
-      versionCode: 148,
+      versionCode: 149,
       adaptiveIcon: {
         backgroundColor: "#ebecff",
         foregroundImage: "app/assets/images/adaptive-foreground.png",
