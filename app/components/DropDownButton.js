e2cb2e9712dd1738df6e495f77365d98461e8aac
RyanTG <gratzer@fastmail.com>
2025-07-10 10:17:44 -0700

    Adjust dropdown button shadow opacity

--- app/components/DropDownButton.js
+++ app/components/DropDownButton.js
@@ -49,7 +49,7 @@ const getStyles = (theme) =>
         width: 0,
         height: 2,
       },
-      shadowOpacity: 0.5,
+      shadowOpacity: 0.25,
       shadowRadius: 3.84,
       elevation: 5,
     },
