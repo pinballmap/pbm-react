420532a2ab03a2b214e9bf47ff812d99d2d7f2e6
RyanTG <gratzer@fastmail.com>
2026-04-19 18:39:53 -0700

    Remove unneeded Metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 const config = getSentryExpoConfig(__dirname);
-config.resolver.unstable_enablePackageExports = false;
 module.exports = config;
