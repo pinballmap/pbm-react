805cc335a3f96bc88314af25102dba46d14c388d
RyanTG <gratzer@fastmail.com>
2026-04-28 11:37:49 -0700

    Add operator bulletpoints to FAQ

--- app/screens/FAQ.js
+++ app/screens/FAQ.js
@@ -147,7 +147,7 @@ const FAQ = ({ navigation }) => {
               />
               <Text style={s.iconText}>Location has 10 or more machines</Text>
             </View>
-            <View style={s.iconView}>
+            <View style={[s.iconView, { paddingBottom: 8 }]}>
               <Image
                 source={require("../assets/images/marker-z-Heart-noShadow.png")}
                 contentFit="fill"
@@ -280,7 +280,7 @@ const FAQ = ({ navigation }) => {
               >
                 {"this form"}
               </Text>
-              {` (or go to the menu icon in the lower right of the map screen and choose "Submit Location"). Our administrators moderate submissions, so please allow 0 - 7 days or so for it to be approved. The more accurate and thorough your submission, the quicker it will get added!`}
+              {` (or go to the menu icon in the lower right of the map screen and choose "Submit Location"). Our administrators moderate submissions, so please allow 0 - 7 days or so for it to be approved. The more accurate and thorough your submission, the quicker it will get added.`}
             </Text>
             <Text
               style={s.bold}
@@ -311,7 +311,7 @@ const FAQ = ({ navigation }) => {
               style={s.bold}
             >{`This location has moved. Should I add a location description that no administrator will ever see? Or should I remove the machines from it and re-submit?`}</Text>
             <Text style={s.text}>
-              {`Neither! Administrators can change the address. Please `}
+              {`Neither. Administrators can change the address. Please `}
               <Text
                 onPress={() => navigation.navigate("Contact")}
                 style={s.textLink}
@@ -362,7 +362,9 @@ const FAQ = ({ navigation }) => {
               >
                 OPDB
               </Text>{" "}
-              {`then we will not include it.\n\n`}
+              {`then we will not include it.`}
+            </Text>
+            <Text style={s.text}>
               {`We do include some pre-pinball flipperless games. Are we missing some?`}
             </Text>
             <Text
@@ -388,11 +390,15 @@ const FAQ = ({ navigation }) => {
               style={s.bold}
             >{`I submitted a new location but it never showed up on the map. What is wrong with you?`}</Text>
             <Text style={s.text}>
-              {`There are numerous reasons why a new location was not added to the map. Here are some:\n\n`}
-              {`\u2022 Not enough time has passed. Please allow 0-7 days before a location is approved. Submissions go to different people, depending on where the location is in the world. And these people work at different paces. Reminder, we're all volunteers with the goal of having a very clean data set.\n\n`}
-
-              {`\u2022 The location you submitted isn't open yet. Many locations are submitted by the business owners themselves (and people helping out those businesses), and we are honored that they think of us when they are prepping to launch their business. But guess who are also eager folks? Pinball players! So if a location gets added to the map before it's open to the world, people will drive all the way there, discover an un-opened business, and then... you guessed it! They then yell at us about it. It doesn't matter if there's a location description saying "Opens on May 5." People do not read (except you).\n\n`}
-
+              {`There are numerous reasons why a new location was not added to the map. Here are some:`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 Not enough time has passed. Please allow 0-7 days before a location is approved. Submissions go to different people, depending on where the location is in the world. And these people work at different paces. Reminder, we're all volunteers with the goal of having a very clean data set.`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 The location you submitted isn't open yet. Many locations are submitted by the business owners themselves (and people helping out those businesses), and we are honored that they think of us when they are prepping to launch their business. But guess who are also eager folks? Pinball players! So if a location gets added to the map before it's open to the world, people will drive all the way there, discover an un-opened business, and then... you guessed it! They then yell at us about it. It doesn't matter if there's a location description saying "Opens on May 5." People do not read (except you).`}
+            </Text>
+            <Text style={s.text}>
               {`\u2022 The location you submitted is a private residence. Or private in some other way. See the question above this.`}
             </Text>
             <View
@@ -402,6 +408,25 @@ const FAQ = ({ navigation }) => {
             >
               <Text style={s.category}>Operators</Text>
             </View>
+            <Text style={[s.text, { paddingTop: 10 }]}>
+              Hundreds of operators are listed on Pinball Map. They use it to
+              easily track and update all of their locations. And also get
+              notified via email when users leave comments on their machines.
+            </Text>
+            <Text style={[s.text]}>
+              {`Check this `}
+              <Text
+                style={s.textLink}
+                onPress={() =>
+                  WebBrowser.openBrowserAsync(
+                    "https://blog.pinballmap.com/2015/10/22/operators-we-want-you/",
+                  )
+                }
+              >
+                blog post
+              </Text>
+              {` about the benefits of being listed in our system, and the tools available. Once you tag all of your locations, you'll be able to quickly see all your locations using the "Filter".`}
+            </Text>
             <Text style={s.bold}>{`How do I get listed as an operator?`}</Text>
             <Text style={s.text}>
               {
@@ -412,8 +437,21 @@ const FAQ = ({ navigation }) => {
                   {"Contact us"}
                 </Text>
               }
-              {` and we'll add you. In your message, tell us the name of your business and whether you want to be notified via email when users leave comments on your machines. Once you tag all of your locations, you'll be able to quickly see all your locations using the "Filter".`}
+              {` and we'll add you. In your message, tell us:`}
             </Text>
+            <Text style={s.text}>{`\u2022 The name of your business.`}</Text>
+            <Text
+              style={s.text}
+            >{`\u2022 Whether you want to be notified via email when users leave comments on your machines (once a day email digest).`}</Text>
+            <Text
+              style={s.text}
+            >{`\u2022 If you have a website, give us the link and your business name will appear as a hyperlink.`}</Text>
+            <Text
+              style={s.text}
+            >{`\u2022 If you want users to be able to directly text/call you (if so, include your number).`}</Text>
+            <Text
+              style={s.text}
+            >{`\u2022 If you want users to be able to directly email you.`}</Text>
             <Text
               style={s.bold}
             >{`How do I tag myself as the operator at a location?`}</Text>
@@ -424,16 +462,24 @@ const FAQ = ({ navigation }) => {
               style={s.bold}
             >{`I'm an operator or business owner, and people are leaving comments on my machines that I don't like. Can you make them stop, or disable comments on my machines?`}</Text>
             <Text style={s.text}>
-              {`The short answer is that we can't disable comments at an individual venue or for an individual user. We understand that users and operators tend to want conflicting things. Some operators find reports of technical issues to be very useful; and some do not. Some operators wish that folks would only leave very specific types of comments (we can't control this). On the other hand, most regular users find machine comments to be a very valuable feature. It is difficult to perfectly cater to everyone, and we cannot control the comments that people make.\n\n`}
-              {`In some circumstances, comments may be removed and/or accounts can be disabled (see the next FAQ items). We will always look into your concerns. But please try not to waste our time with your requests.\n\n`}
+              {`The short answer is that we can't disable comments at an individual venue or for an individual user. We understand that users and operators tend to want conflicting things. Some operators find reports of technical issues to be very useful; and some do not. Some operators wish that folks would only leave very specific types of comments (we can't control this). On the other hand, most regular users find machine comments to be a very valuable feature. It is difficult to perfectly cater to everyone, and we cannot control the comments that people make.`}
+            </Text>
+            <Text style={s.text}>
+              {`In some circumstances, comments may be removed and/or accounts can be disabled (see the next FAQ items). We will always look into your concerns. But please try not to waste our time with your requests.`}
+            </Text>
+            <Text style={s.text}>
               <Text style={{ fontFamily: "Nunito-Bold" }}>
                 To users leaving comments:
               </Text>{" "}
-              {`Most operators prefer you directly tell them about problems. For example, in person or with a note to staff at the venue. Many pinball issues are minor and temporary, and leaving a comment about them on the map puts it "on the record" and it may remain there long after the issue is resolved.\n\n`}
+              {`Most operators prefer you directly tell them about problems. For example, in person or with a note to staff at the venue. Many pinball issues are minor and temporary, and leaving a comment about them on the map puts it "on the record" and it may remain there long after the issue is resolved.`}
+            </Text>
+            <Text style={s.text}>
               <Text style={{ fontFamily: "Nunito-Bold" }}>
                 Some suggestions to operators:
               </Text>{" "}
-              {`A comment about a machine issue is not going to hurt your business in the short term. Pinball Map often gets blamed for "making" operators rush across town to fix and issue and write a comment saying the issue is fixed. Pinball Map does not make you do this.\n\n`}
+              {`A comment about a machine issue is not going to hurt your business in the short term. Pinball Map often gets blamed for "making" operators rush across town to fix and issue and write a comment saying the issue is fixed. Pinball Map does not make you do this.`}
+            </Text>
+            <Text style={s.text}>
               {`According to nearly everyone, it is really valuable to see a history of comments (thing is broken, thing is fixed, etc.). Everyone understands that machines break all the time. Seeing a dialog demonstrates that upkeep is occurring. The "latest comment" noting a problem is not particularly important to users and will not drive them away. Some operators choose to delete machines and re-add them in order to clear out comments rather than simply responding to a comment. This is bad practice - both for your business and for the map - and can result in your account being disabled. It makes it seem like the operator is whitewashing comments rather than fixing issues. Users definitely notice this, and we strongly discourage it.`}
             </Text>
             <Text
@@ -447,7 +493,7 @@ const FAQ = ({ navigation }) => {
             >{`After a machine issue has been resolved, can you remove the machine comment that mentioned the issue?`}</Text>
             <Text
               style={s.text}
-            >{`No. It may seem "bad" for there to be a public record of problems with your machines, but it's actually good! All machines have problems. What we're showing here is a record of you fixing the machines! Users love that, and we think operators should, too!`}</Text>
+            >{`No. It may seem "bad" for there to be a public record of problems with your machines, but it's actually good. All machines have problems, and everyone understands that. What we're showing here is a record of you fixing the machines. Users see that and appreciate it, and we think operators should appreciate it, too.`}</Text>
             <View
               ref={(r) => {
                 sectionRefs.current["users"] = r;
@@ -500,29 +546,37 @@ const FAQ = ({ navigation }) => {
               style={s.bold}
             >{`I left a machine comment, but I made a typo. Or, I just woke up and I regret a comment I made last night. Can I edit or delete my comment?`}</Text>
             <Text style={s.text}>
-              {`Yes. Click the machine and below your comment you'll see "edit" and "delete" buttons.\n\n`}
-              {`You can do the same for scores, too.`}
+              {`Yes. Click the machine and below your comment you'll see "edit" and "delete" buttons.`}
             </Text>
+            <Text style={s.text}>{`You can do the same for scores, too.`}</Text>
             <Text style={s.bold}>{`Why was my comment removed?`}</Text>
             <Text style={s.text}>
-              {`The machine comment field is for machine comments. These comments are useful for operators to be informed of machine issues, and for other users to understand the general condition of machines. When comments stray, the site becomes less useful to both operators and users. Pinball Map administrators may remove your comment as a means of maintaining the site's goals and value.\n\n`}
-              {`For example, if you wrote a comment about a great flavor of potato chips you just discovered, it would be removed (that's not a machine comment and isn't appropriate for this context). Likewise, if you wrote about an upcoming tournament, or treated the machine comments section like a message board or social media site (responding to other users in an off-topic manner for the sake of dialog), or left general trash-talking of operators - those are likely to be removed (those are not a machine comments, detract from the goals, and aren't appropriate for this context). Make sense?\n\n`}
-              {`Additionally, Pinball Map administrators may deem your machine comment (or location description) to be inappropriate because:\n\n`}
-
-              {`\u2022 It is some form of spam. Spam can be defined as writing the same comment on many machines.\n\n`}
-
-              {`\u2022 It is advertising a machine for sale. This is not a marketplace.\n\n`}
-
-              {`\u2022 It contains a personal attack.\n\n`}
-
-              {`\u2022 It is offensive.`}
+              {`The machine comment field is for machine comments. These comments are useful for operators to be informed of machine issues, and for other users to understand the general condition of machines. When comments stray, the site becomes less useful to both operators and users. Pinball Map administrators may remove your comment as a means of maintaining the site's goals and value.`}
             </Text>
+            <Text style={s.text}>
+              {`For example, if you wrote a comment about a great flavor of potato chips you just discovered, it would be removed (that's not a machine comment and isn't appropriate for this context). Likewise, if you wrote about an upcoming tournament, or treated the machine comments section like a message board or social media site (responding to other users in an off-topic manner for the sake of dialog), or left general trash-talking of operators - those are likely to be removed (those are not a machine comments, detract from the goals, and aren't appropriate for this context). Make sense?`}
+            </Text>
+            <Text style={s.text}>
+              {`Additionally, Pinball Map administrators may deem your machine comment (or location description) to be inappropriate because:`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 It is some form of spam. Spam can be defined as writing the same comment on many machines.`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 It is advertising a machine for sale. This is not a marketplace.`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 It contains a personal attack.`}
+            </Text>
+            <Text style={s.text}>{`\u2022 It is offensive.`}</Text>
             <Text style={s.bold}>{`Why was my account disabled?`}</Text>
             <Text style={s.text}>
-              {`\u2022 If your account has been used to mess with data on the site, then we may disable it. Bad behavior includes, but is not limited to: removing machines that are at a location (or removing+adding to clear out comments); adding machines that are not at a location; leaving lots of inappropriate/abusive comments; or adding a machine as a proxy for a non-pinball machine (such as Slugfest) over and over.\n\n`}
-
-              {`\u2022 You created a sockpuppet account to hype your location and/or disparage the competition. This is unethical and it makes the site worse. Don't do this. Look in the mirror.\n\n`}
-
+              {`\u2022 If your account has been used to mess with data on the site, then we may disable it. Bad behavior includes, but is not limited to: removing machines that are at a location (or removing+adding to clear out comments); adding machines that are not at a location; leaving lots of inappropriate/abusive comments; or adding a machine as a proxy for a non-pinball machine (such as Slugfest) over and over.`}
+            </Text>
+            <Text style={s.text}>
+              {`\u2022 You created a sockpuppet account to hype your location and/or disparage the competition. This is unethical and it makes the site worse. Don't do this. Look in the mirror.`}
+            </Text>
+            <Text style={s.text}>
               {`\u2022 You signed up with a disposable email account. We sometimes have legitimate reasons for contacting you individually. Disposable emails are most often used by people who abuse the site.`}
             </Text>
             <View
@@ -556,7 +610,10 @@ const FAQ = ({ navigation }) => {
               >
                 detailed privacy policy on our website
               </Text>
-              {`. The overview: We do not track or store user locations, nor store any personal information. We do not sell any user data. We do not use third-party analytics. This site is not monetized. We keep a log of map edits that users make.\n\nThis app uses Mapbox for the map, and while we set telemetry (`}
+              {`. The overview: We do not track or store user locations, nor store any personal information. We do not sell any user data. We do not use third-party analytics. This site is not monetized. We keep a log of map edits that users make.`}
+            </Text>
+            <Text style={s.text}>
+              {`This app uses Mapbox for the map, and while we set telemetry (`}
               <Text
                 style={s.textLink}
                 onPress={() =>
