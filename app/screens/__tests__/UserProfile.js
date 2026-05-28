0d4016f7f015f47f81f5d02fa7bfb0c26bf65bd1
RyanTG <gratzer@fastmail.com>
2026-05-28 09:54:08 -0700

    Remove unused tests/enzyme

--- app/screens/__tests__/UserProfile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UserProfile from "../UserProfile";
-
-import renderer from "react-test-renderer";
-
-it("renders without crashing", () => {
-  const rendered = renderer.create(<UserProfile />).toJSON();
-  expect(rendered).toBeTruthy();
-});
